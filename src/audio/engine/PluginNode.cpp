1b47ac677ba33fc0f4d0076f6e254051ecdcd481
xris1658 <chris1658@foxmail.com>
2023-02-16 22:53:31 +0800

    Remove `PluginNode` and `PluginCacheTable`

--- src/audio/engine/PluginNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "PluginNode.hpp"
-
-namespace YADAW::Audio::Engine
-{
-void PluginNode::process(const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData)
-{
-    process_(audioProcessData);
-}
-}
