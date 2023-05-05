000e0ff3c56972866d3903723ef982f31c9e2fb3
xris1658 <chris1658@foxmail.com>
2023-05-05 11:50:25 +0800

    Register all model types

--- src/model/ModelInitializer.cpp
+++ src/model/ModelInitializer.cpp
@@ -2,13 +2,25 @@
 
 #include "util/QmlUtil.hpp"
 #include "model/IAssetDirectoryListModel.hpp"
+#include "model/IAudioBusChannelListModel.hpp"
+#include "model/IAudioBusConfigurationModel.hpp"
+#include "model/IAudioGraphDeviceListModel.hpp"
+#include "model/IAutomationModel.hpp"
+#include "model/ILocalizationListModel.hpp"
 #include "model/IPluginDirectoryListModel.hpp"
+#include "model/IPluginListModel.hpp"
 
 namespace YADAW::Model
 {
 void initializeModel()
 {
     QML_REGISTER_TYPE(IAssetDirectoryListModel, YADAW.Models, 1, 0);
+    QML_REGISTER_TYPE(IAudioBusChannelListModel, YADAW.Models, 1, 0);
+    QML_REGISTER_TYPE(IAudioBusConfigurationModel, YADAW.Models, 1, 0);
+    QML_REGISTER_TYPE(IAudioGraphDeviceListModel, YADAW.Models, 1, 0);
+    QML_REGISTER_TYPE(IAutomationModel, YADAW.Models, 1, 0);
+    QML_REGISTER_TYPE(ILocalizationListModel, YADAW.Models, 1, 0);
     QML_REGISTER_TYPE(IPluginDirectoryListModel, YADAW.Models, 1, 0);
+    QML_REGISTER_TYPE(IPluginListModel, YADAW.Models, 1, 0);
 }
 }
