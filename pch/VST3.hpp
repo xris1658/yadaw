134590ca3548938a2b6d3a93f7cd49e89e71822d
xris1658 <chris1658@foxmail.com>
2023-05-07 09:36:38 +0800

    Update PCH of VST3

--- pch/VST3.hpp
+++ pch/VST3.hpp
@@ -1,11 +1,15 @@
 #ifndef YADAW_PCH_VST3
 #define YADAW_PCH_VST3
 
+#include <pluginterfaces/base/ftypes.h>
 #include <pluginterfaces/base/funknown.h>
 #include <pluginterfaces/base/ipluginbase.h>
+#include <pluginterfaces/gui/iplugview.h>
 #include <pluginterfaces/vst/ivstattributes.h>
 #include <pluginterfaces/vst/ivstaudioprocessor.h>
+#include <pluginterfaces/vst/ivstcomponent.h>
 #include <pluginterfaces/vst/ivsteditcontroller.h>
+#include <pluginterfaces/vst/ivstevents.h>
 #include <pluginterfaces/vst/ivsthostapplication.h>
 #include <pluginterfaces/vst/ivstmessage.h>
 #include <pluginterfaces/vst/ivstnoteexpression.h>
@@ -13,5 +17,6 @@
 #include <pluginterfaces/vst/ivstprocesscontext.h>
 #include <pluginterfaces/vst/ivstrepresentation.h>
 #include <pluginterfaces/vst/vstspeaker.h>
+#include <pluginterfaces/vst/vsttypes.h>
 
 #endif //YADAW_PCH_VST3
