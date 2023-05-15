c69e816ca10dda856fd2790a872aee041ee3bdc7
xris1658 <chris1658@foxmail.com>
2023-05-15 09:20:58 +0800

    Attach to and detach with window correctly

--- src/audio/plugin/VestifalPluginGUI.cpp
+++ src/audio/plugin/VestifalPluginGUI.cpp
@@ -22,14 +22,18 @@ bool VestifalPluginGUI::attachToWindow(QWindow* window)
 {
     if(!window_)
     {
-        window_ = window;
-        runDispatcher(effect_, EffectOpcode::effectOpenEditor, 0, 0, reinterpret_cast<void*>(window->winId()));
-        VestifalRectangle* rectangle = nullptr;
-        runDispatcher(effect_, EffectOpcode::effectGetRect, 0, 0, &rectangle);
-        window_->setWidth(rectangle->right - rectangle->left);
-        window_->setHeight(rectangle->bottom - rectangle->top);
-        return true;
+        auto attachResult = runDispatcher(effect_, EffectOpcode::effectOpenEditor, 0, 0, reinterpret_cast<void*>(window->winId()));
+        if(attachResult)
+        {
+            window_ = window;
+            VestifalRectangle* rectangle = nullptr;
+            runDispatcher(effect_, EffectOpcode::effectGetRect, 0, 0, &rectangle);
+            window_->setWidth(rectangle->right - rectangle->left);
+            window_->setHeight(rectangle->bottom - rectangle->top);
+        }
+        return attachResult;
     }
+    return false;
 }
 
 QWindow* VestifalPluginGUI::window()
@@ -39,7 +43,12 @@ QWindow* VestifalPluginGUI::window()
 
 bool VestifalPluginGUI::detachWithWindow()
 {
-    runDispatcher(effect_, EffectOpcode::effectCloseEditor);
-    return true;
+    if(window_)
+    {
+        runDispatcher(effect_, EffectOpcode::effectCloseEditor);
+        window_ = nullptr;
+        return true;
+    }
+    return false;
 }
 }
