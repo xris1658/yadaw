6f77b1eec156893c9eb9ba1b9d6c3b1ad305c52a
xris1658 <chris1658@foxmail.com>
2023-07-14 09:52:58 +0800

    Add constants for remapping from RFC 7845 5.1.1.5

--- src/audio/util/ChannelGroupRemapper.hpp
+++ src/audio/util/ChannelGroupRemapper.hpp
@@ -29,6 +29,248 @@ private:
     AudioChannelGroup outputChannelGroup_;
     std::vector<std::vector<float>> gain_;
 };
-}
 
-#endif //YADAW_SRC_AUDIO_UTIL_CHANNELGROUPCONVERTER
+const float stereoFromMono[2][1] = {
+    {1.0f},
+    {1.0f}
+};
+
+const float monoFromStereo[1][2] = {
+    {
+        0.5f,
+        0.5f
+    }
+};
+
+// https://www.rfc-editor.org/rfc/rfc7845#section-5.1.1.5
+
+const float stereoFromLRC[2][3] = {
+    {
+        1.0f,
+        0.5f * YADAW::Util::sqrt2<float>() - 1.0f,
+        0.0f
+    },
+    {
+        0.0f,
+        0.5f * YADAW::Util::sqrt2<float>() - 1.0f,
+        1.0f
+    }
+};
+
+const float stereoFromLRCNormalization = 2.0f / (2.0f + YADAW::Util::sqrt2<float>());
+
+const float stereoFromLRCNormalized[2][3] = {
+    {
+        stereoFromLRC[0][0] * stereoFromLRCNormalization,
+        stereoFromLRC[0][1] * stereoFromLRCNormalization,
+        stereoFromLRC[0][2] * stereoFromLRCNormalization
+    },
+    {
+        stereoFromLRC[1][0] * stereoFromLRCNormalization,
+        stereoFromLRC[1][1] * stereoFromLRCNormalization,
+        stereoFromLRC[1][2] * stereoFromLRCNormalization
+    }
+};
+
+const float stereoFromQuad[2][4] = {
+    {
+        1.0f,
+        0.0f,
+        YADAW::Util::sqrt3<float>() * 0.5f,
+        0.5f
+    },
+    {
+        0.0f,
+        1.0f,
+        0.5f,
+        YADAW::Util::sqrt3<float>() * 0.5f
+    },
+};
+
+const float stereoFromQuadNormalization = 2.0f / (3.0f + YADAW::Util::sqrt3<float>());
+
+const float stereoFromQuadNormalized[2][4] = {
+    {
+        stereoFromQuad[0][0] * stereoFromQuadNormalization,
+        stereoFromQuad[0][1] * stereoFromQuadNormalization,
+        stereoFromQuad[0][2] * stereoFromQuadNormalization,
+        stereoFromQuad[0][3] * stereoFromQuadNormalization
+    },
+    {
+        stereoFromQuad[1][0] * stereoFromQuadNormalization,
+        stereoFromQuad[1][1] * stereoFromQuadNormalization,
+        stereoFromQuad[1][2] * stereoFromQuadNormalization,
+        stereoFromQuad[1][3] * stereoFromQuadNormalization
+    }
+};
+
+const float stereoFrom50[2][5] = {
+    {
+        1.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        0.0f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f
+    },
+    {
+        0.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        1.0f,
+        0.5f,
+        0.5f * YADAW::Util::sqrt3<float>()
+    }
+};
+
+const float stereoFrom50Normalization = 1.0f / (3.0f + YADAW::Util::sqrt2<float>() + YADAW::Util::sqrt3<float>());
+
+const float stereoFrom50Normalized[2][5] = {
+    {
+        stereoFrom50[0][0] * stereoFrom50Normalization,
+        stereoFrom50[0][1] * stereoFrom50Normalization,
+        stereoFrom50[0][2] * stereoFrom50Normalization,
+        stereoFrom50[0][3] * stereoFrom50Normalization,
+        stereoFrom50[0][4] * stereoFrom50Normalization,
+    },
+    {
+        stereoFrom50[1][0] * stereoFrom50Normalization,
+        stereoFrom50[1][1] * stereoFrom50Normalization,
+        stereoFrom50[1][2] * stereoFrom50Normalization,
+        stereoFrom50[1][3] * stereoFrom50Normalization,
+        stereoFrom50[1][4] * stereoFrom50Normalization,
+    }
+};
+
+const float stereoFrom51[2][6] = {
+    {
+        1.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        0.0f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f,
+        0.5f * YADAW::Util::sqrt2<float>()
+    },
+    {
+        0.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        1.0f,
+        0.5f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f * YADAW::Util::sqrt2<float>()
+    }
+};
+
+const float stereoFrom51Normalization = 1.0f / (3.0f + YADAW::Util::sqrt3<float>() + 2 * YADAW::Util::sqrt2<float>());
+
+const float stereoFrom51Normalized[2][6] = {
+    {
+        stereoFrom51[0][0] * stereoFrom51Normalization,
+        stereoFrom51[0][1] * stereoFrom51Normalization,
+        stereoFrom51[0][2] * stereoFrom51Normalization,
+        stereoFrom51[0][3] * stereoFrom51Normalization,
+        stereoFrom51[0][4] * stereoFrom51Normalization,
+        stereoFrom51[0][5] * stereoFrom51Normalization
+    },
+    {
+        stereoFrom51[1][0] * stereoFrom51Normalization,
+        stereoFrom51[1][1] * stereoFrom51Normalization,
+        stereoFrom51[1][2] * stereoFrom51Normalization,
+        stereoFrom51[1][3] * stereoFrom51Normalization,
+        stereoFrom51[1][4] * stereoFrom51Normalization,
+        stereoFrom51[1][5] * stereoFrom51Normalization
+    }
+};
+
+const float stereoFrom61[2][7] = {
+    {
+        1.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        0.0f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f,
+        YADAW::Util::sqrt2<float>() * YADAW::Util::sqrt3<float>() * 0.25f,
+        0.5f * YADAW::Util::sqrt2<float>()
+    },
+    {
+        0.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        1.0f,
+        0.5f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        YADAW::Util::sqrt2<float>() * YADAW::Util::sqrt3<float>() * 0.25f,
+        0.5f * YADAW::Util::sqrt2<float>()
+    }
+};
+
+const float stereoFrom61Normalization = 1.0f / (3.0f + 2.0f * (YADAW::Util::sqrt3<float>() + YADAW::Util::sqrt2<float>()));
+
+const float stereoFrom61Normalized[2][7] = {
+    {
+        stereoFrom61[0][0] * stereoFrom61Normalization,
+        stereoFrom61[0][1] * stereoFrom61Normalization,
+        stereoFrom61[0][2] * stereoFrom61Normalization,
+        stereoFrom61[0][3] * stereoFrom61Normalization,
+        stereoFrom61[0][4] * stereoFrom61Normalization,
+        stereoFrom61[0][5] * stereoFrom61Normalization,
+        stereoFrom61[0][6] * stereoFrom61Normalization
+    },
+    {
+        stereoFrom61[1][0] * stereoFrom61Normalization,
+        stereoFrom61[1][1] * stereoFrom61Normalization,
+        stereoFrom61[1][2] * stereoFrom61Normalization,
+        stereoFrom61[1][3] * stereoFrom61Normalization,
+        stereoFrom61[1][4] * stereoFrom61Normalization,
+        stereoFrom61[1][5] * stereoFrom61Normalization,
+        stereoFrom61[1][6] * stereoFrom61Normalization
+    }
+};
+
+const float stereoFrom71[2][8] = {
+    {
+        1.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        0.0f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f,
+        0.5f * YADAW::Util::sqrt2<float>()
+    },
+    {
+        0.0f,
+        0.5f * YADAW::Util::sqrt2<float>(),
+        1.0f,
+        0.5f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f,
+        0.5f * YADAW::Util::sqrt3<float>(),
+        0.5f * YADAW::Util::sqrt2<float>()
+    }
+};
+
+const float stereoFrom71Normalization = 2.0f / (2.0f + YADAW::Util::sqrt2<float>() + YADAW::Util::sqrt3<float>());
+
+
+const float stereoFrom71Normalized[2][8] = {
+    {
+        stereoFrom71[0][0] * stereoFrom71Normalization,
+        stereoFrom71[0][1] * stereoFrom71Normalization,
+        stereoFrom71[0][2] * stereoFrom71Normalization,
+        stereoFrom71[0][3] * stereoFrom71Normalization,
+        stereoFrom71[0][4] * stereoFrom71Normalization,
+        stereoFrom71[0][5] * stereoFrom71Normalization,
+        stereoFrom71[0][6] * stereoFrom71Normalization,
+        stereoFrom71[0][7] * stereoFrom71Normalization
+    },
+    {
+        stereoFrom71[1][0] * stereoFrom71Normalization,
+        stereoFrom71[1][1] * stereoFrom71Normalization,
+        stereoFrom71[1][2] * stereoFrom71Normalization,
+        stereoFrom71[1][3] * stereoFrom71Normalization,
+        stereoFrom71[1][4] * stereoFrom71Normalization,
+        stereoFrom71[1][5] * stereoFrom71Normalization,
+        stereoFrom71[1][6] * stereoFrom71Normalization,
+        stereoFrom71[1][7] * stereoFrom71Normalization
+    }
+};
+}
+#endif //YADAW_SRC_AUDIO_UTIL_CHANNELGROUPREMAPPER
--- src/util/Constants.hpp
+++ src/util/Constants.hpp
@@ -21,6 +21,12 @@ constexpr T sqrt2()
 {
     return std::numbers::sqrt2_v<T>;
 }
+
+template<typename T>
+constexpr T sqrt3()
+{
+    return std::numbers::sqrt3_v<T>;
+}
 #else
 
 // Variable template and `constexpr` is not used for several reasons:
@@ -45,7 +51,18 @@ T sqrt2()
     return std::sqrtl(2.0l);
 #endif
 }
+
+template<typename T>
+T sqrt3()
+{
+#if(__GNUC__)
+    return std::sqrt(3.0l);
+#else
+    return std::sqrtl(3.0l);
 #endif
 }
+#endif
+}
+
 
 #endif // YADAW_SRC_UTIL_CONSTANTS
