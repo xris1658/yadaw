00e43aa4498e3e927b3d66f4752b42683420ebf7
xris1658 <chris1658@foxmail.com>
2023-07-27 07:20:05 +0800

    Update MIDI message conversion; match commit 3cf049e4

--- src/midi/MessageToCLAPEvent.cpp
+++ src/midi/MessageToCLAPEvent.cpp
@@ -10,13 +10,13 @@ clap_event_header* createCLAPEventFromMessage(const Message& message)
     if(YADAW::MIDI::isChannelVoiceMessageHeader(data[0]))
     {
         auto header = reinterpret_cast<const YADAW::MIDI::ChannelVoiceMessageHeader*>(data);
-        switch(header->type)
+        switch(header->type())
         {
         case YADAW::MIDI::NoteOnMessage::TypeId:
         {
             auto* ret = reinterpret_cast<clap_event_note*>(std::malloc(sizeof(clap_event_note)));
             auto* noteOn = reinterpret_cast<const YADAW::MIDI::NoteOnMessage*>(data);
-            ret->channel = noteOn->header.channel;
+            ret->channel = noteOn->header.channel();
             ret->note_id = -1;
             ret->key = noteOn->note;
             ret->velocity = noteOn->velocity / 127.0;
@@ -26,7 +26,7 @@ clap_event_header* createCLAPEventFromMessage(const Message& message)
         {
             auto* ret = reinterpret_cast<clap_event_note*>(std::malloc(sizeof(clap_event_note)));
             auto* noteOff = reinterpret_cast<const YADAW::MIDI::NoteOffMessage*>(data);
-            ret->channel = noteOff->header.channel;
+            ret->channel = noteOff->header.channel();
             ret->note_id = -1;
             ret->key = noteOff->note;
             ret->velocity = noteOff->velocity / 127.0;
--- src/midi/MessageToVST3Event.cpp
+++ src/midi/MessageToVST3Event.cpp
@@ -12,12 +12,12 @@ Event createVST3EventFromMessage(const Message& message)
     if(YADAW::MIDI::isChannelVoiceMessageHeader(data[0]))
     {
         auto header = reinterpret_cast<const YADAW::MIDI::ChannelVoiceMessageHeader*>(data);
-        switch(header->type)
+        switch(header->type())
         {
         case YADAW::MIDI::NoteOnMessage::TypeId:
         {
             auto* noteOn = reinterpret_cast<const YADAW::MIDI::NoteOnMessage*>(data);
-            ret.noteOn.channel = noteOn->header.channel;
+            ret.noteOn.channel = noteOn->header.channel();
             ret.noteOn.noteId = -1;
             ret.noteOn.pitch = noteOn->note;
             ret.noteOn.velocity = noteOn->velocity / 127.0;
@@ -28,7 +28,7 @@ Event createVST3EventFromMessage(const Message& message)
         case YADAW::MIDI::NoteOffMessage::TypeId:
         {
             auto* noteOff = reinterpret_cast<const YADAW::MIDI::NoteOffMessage*>(data);
-            ret.noteOff.channel = noteOff->header.channel;
+            ret.noteOff.channel = noteOff->header.channel();
             ret.noteOff.noteId = -1;
             ret.noteOff.pitch = noteOff->note;
             ret.noteOff.velocity = noteOff->velocity / 127.0;
@@ -38,7 +38,7 @@ Event createVST3EventFromMessage(const Message& message)
         case YADAW::MIDI::PolyKeyPressureMessage::TypeId:
         {
             auto* polyKeyPressure = reinterpret_cast<const YADAW::MIDI::PolyKeyPressureMessage*>(data);
-            ret.polyPressure.channel = polyKeyPressure->header.channel;
+            ret.polyPressure.channel = polyKeyPressure->header.channel();
             ret.polyPressure.noteId = -1;
             ret.polyPressure.pitch = polyKeyPressure->note;
             ret.polyPressure.pressure = polyKeyPressure->pressure;
