3aba2e2cd54379fd23d24f85eeab79f786fe5322
xris1658 <chris1658@foxmail.com>
2023-07-29 10:58:30 +0800

    Update MIDI callback

--- test/WindowsMIDIInputDeviceTest/main.cpp
+++ test/WindowsMIDIInputDeviceTest/main.cpp
@@ -1,74 +1,6 @@
 #include "midi/MIDIInputDevice.hpp"
-#include "util/Literal.hpp"
 
-#include <cstdio>
-
-void printTime(const YADAW::MIDI::MIDIInputDevice& device, const YADAW::MIDI::Message& message)
-{
-    using namespace YADAW::MIDI;
-    using namespace YADAW::Util;
-    auto header = reinterpret_cast<ChannelVoiceMessageHeader*>(message.data);
-    std::printf("Channel: %hhu\n", header->channel());
-    std::printf("   Type: %hhu\n", header->type());
-    switch(header->type())
-    {
-    case NoteOffMessage::TypeId:
-    {
-        auto noteOff = reinterpret_cast<NoteOffMessage*>(message.data);
-        auto note = noteOff->note;
-        auto noteName = getNoteName<char>(note, MIDINoteNameKeyShift::kSharp, MIDINoteOctaveOffset::kNoOffset);
-        std::printf("      Note: %s\n", noteName.data());
-        std::printf("  Velocity: %hhu\n", noteOff->velocity);
-        break;
-    }
-    case NoteOnMessage::TypeId:
-    {
-        auto noteOn = reinterpret_cast<NoteOnMessage*>(message.data);
-        auto note = noteOn->note;
-        auto noteName = getNoteName<char>(note, MIDINoteNameKeyShift::kSharp, MIDINoteOctaveOffset::kNoOffset);
-        std::printf("      Note: %s\n", noteName.data());
-        std::printf("  Velocity: %hhu\n", noteOn->velocity);
-        break;
-    }
-    case PolyKeyPressureMessage::TypeId:
-    {
-        auto pkp = reinterpret_cast<PolyKeyPressureMessage*>(message.data);
-        auto note = pkp->note;
-        auto noteName = getNoteName<char>(note, MIDINoteNameKeyShift::kSharp, MIDINoteOctaveOffset::kNoOffset);
-        std::printf("      Note: %s\n", noteName.data());
-        std::printf("  Pressure: %hhu\n", pkp->pressure);
-        break;
-    }
-    case ControlChangeMessage::TypeId:
-    {
-        auto cc = reinterpret_cast<ControlChangeMessage*>(message.data);
-        std::printf("  Ctrl. ID: %hhu\n", cc->controlId);
-        std::printf("     Value: %hhu\n", cc->controlValue);
-        break;
-    }
-    case ProgramChangeMessage::TypeId:
-    {
-        auto pc = reinterpret_cast<ProgramChangeMessage*>(message.data);
-        std::printf("  Prog. ID: %hhu\n", pc->programId);
-        break;
-    }
-    case ChannelPressureMessage::TypeId:
-    {
-        auto cp = reinterpret_cast<ChannelPressureMessage*>(message.data);
-        std::printf("  Pressure: %hhu\n", cp->pressure);
-        break;
-    }
-    case PitchBendChangeMessage::TypeId:
-    {
-        auto pb = reinterpret_cast<PitchBendChangeMessage*>(message.data);
-        std::printf("  PB Value: %hd\n", pb->value());
-        break;
-    }
-    default:
-        break;
-    }
-    std::printf("   Time: %lld\n", message.timestampInNanoseconds);
-}
+#include "test/common/MIDICallback.hpp"
 
 int main()
 {
@@ -95,7 +27,7 @@ int main()
     }
     getchar();
     YADAW::MIDI::MIDIInputDevice device(MIDIInputDevice::inputDeviceAt(index - 1)->id);
-    device.start(&printTime);
+    device.start(&midiCallback);
     getchar();
     device.stop();
 }
---
+++ test/common/MIDICallback.hpp
@@ -0,0 +1,9 @@
+#ifndef YADAW_TEST_COMMON_MIDICALLBACK
+#define YADAW_TEST_COMMON_MIDICALLBACK
+
+#include "midi/Message.hpp"
+#include "midi/MIDIInputDevice.hpp"
+
+void midiCallback(const YADAW::MIDI::MIDIInputDevice& device, const YADAW::MIDI::Message& message);
+
+#endif //YADAW_TEST_COMMON_MIDICALLBACK
