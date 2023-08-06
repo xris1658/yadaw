98af1e0000e7316b542d8cd48c6fca6d02b028e3
xris1658 <chris1658@foxmail.com>
2023-08-06 14:03:51 +0800

    Add `IAudioChannelGroupModel`

---
+++ content/model/IAudioChannelGroupModel.hpp
@@ -0,0 +1,42 @@
+#ifndef YADAW_CONTENT_MODEL_IAUDIOCHANNELGROUPMODEL
+#define YADAW_CONTENT_MODEL_IAUDIOCHANNELGROUPMODEL
+
+#include "ModelBase.hpp"
+
+#include <QAbstractListModel>
+
+namespace YADAW::Model
+{
+class IAudioChannelGroupModel: public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    enum Role
+    {
+        ChannelType,
+        Name,
+        IsMain,
+        RoleCount
+    };
+public:
+    IAudioChannelGroupModel(QObject* parent = nullptr): QAbstractListModel(parent) {}
+    virtual ~IAudioChannelGroupModel() {}
+public:
+    static constexpr int roleCount() { return RoleCount - Qt::UserRole; }
+    static constexpr int columnCount() { return 1; }
+    int columnCount(const QModelIndex&) const override final { return columnCount(); }
+protected:
+    RoleNames roleNames() const override
+    {
+        static RoleNames ret
+        {
+            std::make_pair(Role::ChannelType, "acgm_channel_type"),
+            std::make_pair(Role::Name, "acgm_name"),
+            std::make_pair(Role::IsMain, "acgm_is_main")
+        };
+        return ret;
+    }
+};
+}
+
+#endif // YADAW_CONTENT_MODEL_IAUDIOCHANNELGROUPMODEL
