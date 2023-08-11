11e9d041ee0579f48fee7c1205da512c74bf7587
xris1658 <chris1658@foxmail.com>
2023-08-11 20:04:34 +0800

    Update `AudioProcessData` helper classes
    - Rename `AudioProcessDataContainer` to `AudioProcessDataBufferContainer`
    - Use `std::shared_ptr<AudioBufferPool::Buffer>` to reuse and manage buffers easily

---
+++ src/audio/engine/AudioProcessDataBufferContainer.hpp
@@ -0,0 +1,76 @@
+#ifndef YADAW_SRC_AUDIO_ENGINE_AUDIOPROCESSDATABUFFERCONTAINER
+#define YADAW_SRC_AUDIO_ENGINE_AUDIOPROCESSDATABUFFERCONTAINER
+
+#include "audio/device/IAudioDevice.hpp"
+#include "audio/util/AudioBufferPool.hpp"
+#include "audio/util/AudioProcessDataPointerContainer.hpp"
+
+#include <memory>
+#include <vector>
+
+namespace YADAW::Audio::Engine
+{
+template<typename SampleType>
+class AudioProcessDataBufferContainer
+{
+public:
+    void setSingleBufferSize(std::uint32_t singleBufferSize)
+    {
+        container_.setSingleBufferSize(singleBufferSize);
+    }
+    void setInputGroupCount(std::uint32_t inputGroupCount)
+    {
+        container_.setInputGroupCount(inputGroupCount);
+        inputBuffers_.resize(inputGroupCount);
+    }
+    void setOutputGroupCount(std::uint32_t outputGroupCount)
+    {
+        container_.setOutputGroupCount(outputGroupCount);
+        outputBuffers_.resize(outputGroupCount);
+    }
+    void setInputCount(std::uint32_t groupIndex, std::uint32_t inputCount)
+    {
+        container_.setInputCount(groupIndex, inputCount);
+        inputBuffers_[groupIndex].resize(inputCount);
+    }
+    void setOutputCount(std::uint32_t groupIndex, std::uint32_t outputCount)
+    {
+        container_.setOutputCount(groupIndex, outputCount);
+        outputBuffers_[groupIndex].resize(outputCount);
+    }
+    std::shared_ptr<YADAW::Audio::Util::AudioBufferPool::Buffer>
+        inputBuffer(std::uint32_t groupIndex, std::uint32_t channelIndex)
+    {
+        return inputBuffers_[groupIndex][channelIndex];
+    }
+    std::shared_ptr<YADAW::Audio::Util::AudioBufferPool::Buffer>
+        outputBuffer(std::uint32_t groupIndex, std::uint32_t channelIndex)
+    {
+        return outputBuffers_[groupIndex][channelIndex];
+    }
+    void setInputBuffer(std::uint32_t groupIndex, std::uint32_t channelIndex,
+        std::shared_ptr<YADAW::Audio::Util::AudioBufferPool::Buffer> buffer)
+    {
+        inputBuffers_[groupIndex][channelIndex] = std::move(buffer);
+        container_.setInput(groupIndex, channelIndex,
+            reinterpret_cast<SampleType*>(buffer.get()));
+    }
+    void setOutputBuffer(std::uint32_t groupIndex, std::uint32_t channelIndex,
+        std::shared_ptr<YADAW::Audio::Util::AudioBufferPool::Buffer> buffer)
+    {
+        outputBuffers_[groupIndex][channelIndex] = std::move(buffer);
+        container_.setOutput(groupIndex, channelIndex,
+            reinterpret_cast<SampleType*>(buffer.get()));
+    }
+    const YADAW::Audio::Device::AudioProcessData<SampleType>& audioProcessData() const
+    {
+        return container_.audioProcessData();
+    }
+private:
+    YADAW::Audio::Util::AudioProcessDataPointerContainer<SampleType> container_;
+    std::vector<std::vector<std::shared_ptr<YADAW::Audio::Util::AudioBufferPool::Buffer>>> inputBuffers_;
+    std::vector<std::vector<std::shared_ptr<YADAW::Audio::Util::AudioBufferPool::Buffer>>> outputBuffers_;
+};
+}
+
+#endif // YADAW_SRC_AUDIO_ENGINE_AUDIOPROCESSDATABUFFERCONTAINER
---
+++ test/AudioProcessDataBufferContainerTest/main.cpp
@@ -0,0 +1,45 @@
+#include "audio/util/AudioBufferPool.hpp"
+#include "audio/engine/AudioProcessDataBufferContainer.hpp"
+
+int main()
+{
+    constexpr auto singleBufferSize = 64;
+    auto audioBufferPool = YADAW::Audio::Util::AudioBufferPool::createPool(singleBufferSize * sizeof(float));
+    auto container = YADAW::Audio::Engine::AudioProcessDataBufferContainer<float>();
+    container.setSingleBufferSize(singleBufferSize);
+    const auto& audioProcessData = container.audioProcessData();
+    container.setInputGroupCount(2);
+    container.setOutputGroupCount(1);
+    for(int i = 0; i < audioProcessData.inputGroupCount; ++i)
+    {
+        container.setInputCount(i, 2);
+        for(int j = 0; j < audioProcessData.inputCounts[i]; ++j)
+        {
+            container.setInputBuffer(i, j,
+                std::make_shared<YADAW::Audio::Util::AudioBufferPool::Buffer>(
+                    audioBufferPool->lend()
+                )
+            );
+        }
+    }
+    for(int i = 0; i < audioProcessData.outputGroupCount; ++i)
+    {
+        container.setOutputCount(i, 1);
+        for(int j = 0; j < audioProcessData.outputCounts[i]; ++j)
+        {
+            container.setOutputBuffer(i, j,
+                container.inputBuffer(i, j)
+            );
+        }
+    }
+    for(int i = 0; i < audioProcessData.inputGroupCount; ++i)
+    {
+        for(int j = 0; j < audioProcessData.inputCounts[i]; ++j)
+        {
+            for(int k = 0; k < audioProcessData.singleBufferSize; ++k)
+            {
+                audioProcessData.inputs[i][j][k] = 1.0;
+            }
+        }
+    }
+}
