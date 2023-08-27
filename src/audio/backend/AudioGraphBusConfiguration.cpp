a74d2efb3169b73813c48016d5b92bc86962699e
xris1658 <chris1658@foxmail.com>
2023-08-27 23:30:28 +0800

    Implement `IAudioDevice` in `AudioGraphBusConfiguration::Bus`

--- src/audio/backend/AudioGraphBusConfiguration.cpp
+++ src/audio/backend/AudioGraphBusConfiguration.cpp
@@ -2,16 +2,33 @@
 
 #include "AudioGraphBusConfiguration.hpp"
 
+#include "util/IntegerRange.hpp"
+
 namespace YADAW::Audio::Backend
 {
 using YADAW::Audio::Device::Channel;
+using YADAW::Audio::Device::IAudioDevice;
 using YADAW::Audio::Device::IBus;
 using ChannelPosition = YADAW::Audio::Device::IAudioBusConfiguration::ChannelPosition;
 
-AudioGraphBusConfiguration::Bus::Bus(std::uint32_t channelCount):
+AudioGraphBusConfiguration::Bus::Bus(
+    bool isInput, std::uint32_t channelCount):
     IBus(channelCount),
-    channels_(channelCount)
-{}
+    channels_(channelCount),
+    isInput_(isInput),
+    audioChannelGroup_()
+{
+    audioChannelGroup_.setChannelGroupType(
+        YADAW::Audio::Base::ChannelGroupType::eCustomGroup,
+        channelCount);
+    audioChannelGroup_.setMain(true);
+    FOR_RANGE0(i, channelCount)
+    {
+        audioChannelGroup_.setSpeakerType(
+            i, YADAW::Audio::Base::ChannelType::Custom);
+        audioChannelGroup_.setSpeakerName(i, QString("N/A"));
+    }
+}
 
 std::optional<Channel> AudioGraphBusConfiguration::Bus::channelAt(std::uint32_t index) const
 {
@@ -25,9 +42,113 @@ bool AudioGraphBusConfiguration::Bus::setChannel(std::uint32_t index, Channel ch
     if(index < channels_.size())
     {
         channels_[index] = channel;
+        audioChannelGroup_.setSpeakerName(index,
+            QString("Device %1, Channel %2")
+            .arg(channel.deviceIndex)
+            .arg(channel.channelIndex)
+        );
         return true;
     }
     return false;
+}
+
+std::uint32_t AudioGraphBusConfiguration::Bus::audioInputGroupCount() const
+{
+    return isInput_? 0: 1;
+}
+
+std::uint32_t AudioGraphBusConfiguration::Bus::audioOutputGroupCount() const
+{
+    return isInput_? 1: 0;
+}
+
+IAudioDevice::OptionalAudioChannelGroup AudioGraphBusConfiguration::Bus::audioInputGroupAt(std::uint32_t index) const
+{
+    return index < audioInputGroupCount()?
+        OptionalAudioChannelGroup(std::ref(audioChannelGroup_)):
+        std::nullopt;
+}
+
+IAudioDevice::OptionalAudioChannelGroup AudioGraphBusConfiguration::Bus::audioOutputGroupAt(std::uint32_t index) const
+{
+    return index < audioOutputGroupCount()?
+        OptionalAudioChannelGroup(std::ref(audioChannelGroup_)):
+        std::nullopt;
+}
+
+std::uint32_t AudioGraphBusConfiguration::Bus::latencyInSamples() const
+{
+    return 0;
+}
+
+using ProcessFunc = void(
+    const AudioGraphBackend::InterleaveAudioBuffer* buffers,
+    const std::vector<YADAW::Audio::Device::Channel>& channels,
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData);
+
+void processInputs(
+    const AudioGraphBackend::InterleaveAudioBuffer* buffers,
+    const std::vector<YADAW::Audio::Device::Channel>& channels,
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData)
+{
+    FOR_RANGE0(i, audioProcessData.inputGroupCount)
+    {
+        FOR_RANGE0(j, audioProcessData.inputCounts[i])
+        {
+            FOR_RANGE0(k, audioProcessData.singleBufferSize)
+            {
+                std::memcpy(
+                    audioProcessData.inputs[i][j] + k,
+                    buffers[channels[i].deviceIndex].at(k, channels[i].channelIndex),
+                    sizeof(float)
+                );
+            }
+        }
+    }
+}
+
+void processOutputs(
+    const AudioGraphBackend::InterleaveAudioBuffer* buffers,
+    const std::vector<YADAW::Audio::Device::Channel>& channels,
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData)
+{
+    FOR_RANGE0(i, audioProcessData.inputGroupCount)
+    {
+        FOR_RANGE0(j, audioProcessData.inputCounts[i])
+        {
+            FOR_RANGE0(k, audioProcessData.singleBufferSize)
+            {
+                std::memcpy(buffers[0].at(k, channels[j].channelIndex),
+                    audioProcessData.inputs[i][j] + k,
+                    sizeof(float)
+                );
+            }
+        }
+    }
+}
+
+ProcessFunc* doProcessFunc[2] = {&processOutputs, &processInputs};
+
+void AudioGraphBusConfiguration::Bus::process(
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData)
+{
+    doProcessFunc[isInput_](buffers_, channels_, audioProcessData);
+}
+
+void AudioGraphBusConfiguration::Bus::setName(const QString& name)
+{
+    audioChannelGroup_.setName(name);
+}
+
+void AudioGraphBusConfiguration::Bus::setName(QString&& name)
+{
+    audioChannelGroup_.setName(std::move(name));
+}
+
+void AudioGraphBusConfiguration::Bus::setInterleaveAudioBuffers(
+    const YADAW::Audio::Backend::AudioGraphBackend::InterleaveAudioBuffer* buffers)
+{
+    buffers_ = buffers;
 }
 
 AudioGraphBusConfiguration::AudioGraphBusConfiguration(const AudioGraphBackend& backend):
@@ -89,7 +210,7 @@ ChannelPosition AudioGraphBusConfiguration::channelPosition(bool isInput, Channe
 uint32_t AudioGraphBusConfiguration::appendBus(bool isInput, std::uint32_t channelCount)
 {
     auto& bus = isInput? inputBusses_: outputBusses_;
-    bus.emplace_back(channelCount);
+    bus.emplace_back(isInput, channelCount);
     return bus.size() - 1;
 }
 
@@ -108,6 +229,43 @@ void AudioGraphBusConfiguration::clearBus(bool isInput)
 {
     auto& busses = isInput? inputBusses_: outputBusses_;
     busses.clear();
+}
+
+OptionalRef<const AudioGraphBusConfiguration::Bus>
+    AudioGraphBusConfiguration::getInputBusAt(std::uint32_t index) const
+{
+    return index < inputBusCount()? OptionalRef<const Bus>(inputBusses_[index]): std::nullopt;
+}
+
+OptionalRef<const AudioGraphBusConfiguration::Bus>
+    AudioGraphBusConfiguration::getOutputBusAt(std::uint32_t index) const
+{
+    return index < outputBusCount()? OptionalRef<const Bus>(outputBusses_[index]): std::nullopt;
+}
+
+OptionalRef<AudioGraphBusConfiguration::Bus>
+    AudioGraphBusConfiguration::getInputBusAt(std::uint32_t index)
+{
+    return index < inputBusCount()? OptionalRef<Bus>(inputBusses_[index]): std::nullopt;
+}
+
+OptionalRef<AudioGraphBusConfiguration::Bus>
+    AudioGraphBusConfiguration::getOutputBusAt(std::uint32_t index)
+{
+    return index < outputBusCount()? OptionalRef<Bus>(outputBusses_[index]): std::nullopt;
+}
+
+void AudioGraphBusConfiguration::setBuffers(const AudioGraphBackend::InterleaveAudioBuffer* inputs,
+    const AudioGraphBackend::InterleaveAudioBuffer* outputs)
+{
+    for(auto& bus: inputBusses_)
+    {
+        bus.setInterleaveAudioBuffers(inputs);
+    }
+    for(auto& bus: outputBusses_)
+    {
+        bus.setInterleaveAudioBuffers(outputs);
+    }
 }
 }
 
--- src/audio/backend/AudioGraphBusConfiguration.hpp
+++ src/audio/backend/AudioGraphBusConfiguration.hpp
@@ -5,6 +5,8 @@
 
 #include "audio/backend/AudioGraphBackend.hpp"
 #include "audio/device/IAudioBusConfiguration.hpp"
+#include "audio/device/IAudioDevice.hpp"
+#include "audio/util/AudioChannelGroup.hpp"
 #include "util/OptionalUtil.hpp"
 
 #include <optional>
@@ -16,15 +18,30 @@ using YADAW::Audio::Device::IBus;
 class AudioGraphBusConfiguration: public YADAW::Audio::Device::IAudioBusConfiguration
 {
 public:
-    class Bus: public YADAW::Audio::Device::IBus
+    class Bus: public YADAW::Audio::Device::IBus, public YADAW::Audio::Device::IAudioDevice
     {
     public:
-        Bus(std::uint32_t channelCount);
-    public:
+        Bus(bool isInput, std::uint32_t channelCount);
+    public: // IBus interfaces
         std::optional<Channel> channelAt(std::uint32_t index) const override;
         bool setChannel(std::uint32_t index, Channel channel) override;
+    public: // IAudioDevice interfaces
+        std::uint32_t audioInputGroupCount() const override;
+        std::uint32_t audioOutputGroupCount() const override;
+        OptionalAudioChannelGroup audioInputGroupAt(std::uint32_t index) const override;
+        OptionalAudioChannelGroup audioOutputGroupAt(std::uint32_t index) const override;
+        std::uint32_t latencyInSamples() const override;
+        void process(const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData) override;
+    public:
+        void setName(const QString& name);
+        void setName(QString&& name);
+        void setInterleaveAudioBuffers(
+            const YADAW::Audio::Backend::AudioGraphBackend::InterleaveAudioBuffer* buffers);
     private:
         std::vector<Channel> channels_;
+        const YADAW::Audio::Backend::AudioGraphBackend::InterleaveAudioBuffer* buffers_;
+        bool isInput_;
+        YADAW::Audio::Util::AudioChannelGroup audioChannelGroup_;
     };
 public:
     AudioGraphBusConfiguration(const AudioGraphBackend& backend);
@@ -39,6 +56,14 @@ public:
     uint32_t appendBus(bool isInput, std::uint32_t channelCount) override;
     bool removeBus(bool isInput, std::uint32_t index) override;
     void clearBus(bool isInput) override;
+public:
+    OptionalRef<const Bus> getInputBusAt(std::uint32_t index) const;
+    OptionalRef<const Bus> getOutputBusAt(std::uint32_t index) const;
+    OptionalRef<Bus> getInputBusAt(std::uint32_t index);
+    OptionalRef<Bus> getOutputBusAt(std::uint32_t index);
+public:
+    void setBuffers(const AudioGraphBackend::InterleaveAudioBuffer* inputs,
+        const AudioGraphBackend::InterleaveAudioBuffer* outputs);
 private:
     const AudioGraphBackend* backend_;
     std::vector<Bus> inputBusses_;
