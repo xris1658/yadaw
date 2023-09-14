ed01b23abc10e02c17c66a6400b7036be254fc85
xris1658 <chris1658@foxmail.com>
2023-09-14 10:41:22 +0800

    Test `ALSABusConfiguraion` in `ALSATest`

--- test/ALSATest/main.cpp
+++ test/ALSATest/main.cpp
@@ -1,4 +1,8 @@
 #include "audio/backend/ALSABackend.hpp"
+#include "audio/backend/ALSABusConfiguration.hpp"
+#include "audio/engine/AudioDeviceGraph.hpp"
+#include "audio/engine/AudioProcessDataBufferContainer.hpp"
+#include "audio/util/AudioBufferPool.hpp"
 #include "midi/MIDIInputDevice.hpp"
 #include "midi/MIDIOutputDevice.hpp"
 #include "native/linux/ALSADeviceEnumerator.hpp"
@@ -8,7 +12,13 @@
 
 #include <cmath>
 #include <cstdio>
+#include <execution>
 #include <thread>
+
+YADAW::Audio::Backend::ALSABusConfiguration* alsaBusConfiguration = nullptr;
+YADAW::Audio::Engine::AudioDeviceGraph* audioDeviceGraph = nullptr;
+
+YADAW::Audio::Engine::AudioDeviceGraph::TopologicalSortResult topologicalSortResult;
 
 std::uint64_t totalFrameCount = 0;
 
@@ -55,16 +65,24 @@ int main(int argc, char** argv)
     }
     std::printf("\n\n");
     YADAW::Audio::Backend::ALSABackend backend;
-    backend.initialize(44100, 512);
+    auto frameCount = 512U;
+    backend.initialize(44100, frameCount);
+    auto pool = YADAW::Audio::Util::AudioBufferPool::createPool<float>(frameCount);
     YADAW::Audio::Backend::ALSADeviceSelector selector(0, 0);
     auto inputIndex = YADAW::Audio::Backend::findDeviceBySelector(backend, true, selector);
+    auto inputChannelCount = 0U;
     auto outputIndex = findDeviceBySelector(backend, false, selector);
+    auto outputChannelCount = 0U;
     if(inputIndex.has_value())
     {
         auto activateInputResult = backend.setAudioDeviceActivated(true, *inputIndex, true);
         if(activateInputResult == decltype(activateInputResult)::Failed)
         {
             std::printf("Activate default input failed!\n");
+        }
+        else
+        {
+            inputChannelCount = backend.channelCount(true, *inputIndex);
         }
     }
     if(outputIndex.has_value())
@@ -74,260 +92,63 @@ int main(int argc, char** argv)
         {
             std::printf("Activate default output failed!\n");
         }
+        else
+        {
+            outputChannelCount = backend.channelCount(false, *outputIndex);
+        }
     }
+    YADAW::Audio::Backend::ALSABusConfiguration busConfiguration(backend);
+    alsaBusConfiguration = &busConfiguration;
+    auto& inputBus = busConfiguration.getInputBusAt(busConfiguration.appendBus(true, inputChannelCount))->get();
+    auto& outputBus = busConfiguration.getOutputBusAt(busConfiguration.appendBus(false, outputChannelCount))->get();
+    YADAW::Audio::Engine::AudioDeviceGraph graph;
+    audioDeviceGraph = &graph;
+    YADAW::Audio::Engine::AudioProcessDataBufferContainer<float> inputProcessDataContainer;
+    inputProcessDataContainer.setSingleBufferSize(frameCount);
+    inputProcessDataContainer.setOutputGroupCount(1);
+    inputProcessDataContainer.setOutputCount(0, inputChannelCount);
+    FOR_RANGE0(i, inputChannelCount)
+    {
+        inputBus.setChannel(i, {*inputIndex, i});
+        inputProcessDataContainer.setOutputBuffer(0, i, std::make_shared<YADAW::Audio::Util::AudioBufferPool::Buffer>(pool->lend()));
+    }
+    YADAW::Audio::Engine::AudioProcessDataBufferContainer<float> outputProcessDataContainer;
+    outputProcessDataContainer.setSingleBufferSize(frameCount);
+    outputProcessDataContainer.setInputGroupCount(1);
+    outputProcessDataContainer.setInputCount(0, outputChannelCount);
+    FOR_RANGE0(i, outputChannelCount)
+    {
+        outputBus.setChannel(i, {*outputIndex, i});
+        outputProcessDataContainer.setInputBuffer(0, i,
+            i < inputChannelCount?
+                inputProcessDataContainer.outputBuffer(0, i):
+                std::make_shared<YADAW::Audio::Util::AudioBufferPool::Buffer>(pool->lend())
+        );
+    }
+    auto inputNode = graph.addNode(YADAW::Audio::Engine::AudioDeviceProcess(inputBus), inputProcessDataContainer.audioProcessData());
+    auto outputNode = graph.addNode(YADAW::Audio::Engine::AudioDeviceProcess(outputBus), outputProcessDataContainer.audioProcessData());
+    graph.connect(inputNode, outputNode, 0, 0);
+    topologicalSortResult = graph.topologicalSort();
     backend.start(callback);
     getchar();
     backend.stop();
 }
 
-auto freq = 440.0f;
-auto ___2pi = 2.0f * std::acos(-1.0f);
-
 void callback(const ALSABackend* backend,
     std::uint32_t inputCount, ALSABackend::AudioBuffer* inputBuffers,
     std::uint32_t outputCount, ALSABackend::AudioBuffer* outputBuffers)
 {
-    auto frameCount = backend->frameCount();
-    auto cycle = backend->sampleRate() / freq;
-    auto data = static_cast<float*>(alloca(sizeof(float) * frameCount));
-    FOR_RANGE0(i, frameCount)
+    alsaBusConfiguration->setBuffers(inputBuffers, outputBuffers);
+    for(auto& row: topologicalSortResult)
     {
-        data[i] = std::sin(static_cast<float>(___2pi * (totalFrameCount + i) / cycle));
-    }
-    if(outputCount > 0)
-    {
-        auto& [format, interleaved, channelCount, buffer] = outputBuffers[0];
-        if(interleaved)
-        {
-            auto interleavedBuffer = static_cast<std::byte*>(buffer.interleavedBuffer);
-            FOR_RANGE0(i, frameCount)
+        std::for_each(std::execution::par_unseq, row.begin(), row.end(),
+            [](std::vector<YADAW::Audio::Engine::AudioDeviceGraph::AudioDeviceProcessNode>& cell)
             {
-                switch(format)
+                for(auto& item: cell)
                 {
-                case ALSABackend::SampleFormat::DoubleFloat:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        *(reinterpret_cast<double*>(interleavedBuffer) + (i * channelCount + j)) = data[i];
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::DoubleFloatRE:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        auto ptr = reinterpret_cast<double*>(interleavedBuffer) + (i * channelCount + j);
-                        *ptr = data[i];
-                        YADAW::Util::reverseEndianness(ptr, sizeof(double));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Float:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        *(reinterpret_cast<float*>(interleavedBuffer) + (i * channelCount + j)) = data[i];
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::FloatRE:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        auto ptr = reinterpret_cast<float*>(interleavedBuffer) + (i * channelCount + j);
-                        *ptr = data[i];
-                        YADAW::Util::reverseEndianness(ptr, sizeof(float));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int32:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        *(reinterpret_cast<std::int32_t*>(interleavedBuffer) + (i * channelCount + j)) = (
-                            static_cast<double>(data[i]) * static_cast<double>(0x80000000) - data[i]);
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int32RE:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        auto ptr = reinterpret_cast<std::int32_t*>(interleavedBuffer) + (i * channelCount + j);
-                        *ptr = (static_cast<double>(data[i]) * static_cast<double>(0x80000000) - data[i]);
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int32_t));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int24:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        *(reinterpret_cast<std::int32_t*>(interleavedBuffer) + (i * channelCount + j)) = (
-                            data[i] * static_cast<float>(0x007fffff));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int24RE:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        auto ptr = reinterpret_cast<std::int32_t*>(interleavedBuffer) + (i * channelCount + j);
-                        *ptr = (data[i] * static_cast<float>(0x007fffff));
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int32_t));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int20:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        *(reinterpret_cast<std::int32_t*>(interleavedBuffer) + (i * channelCount + j)) = (
-                            data[i] * static_cast<float>(0x0007ffff));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int20RE:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        auto ptr = reinterpret_cast<std::int32_t*>(interleavedBuffer) + (i * channelCount + j);
-                        *ptr = (data[i] * static_cast<float>(0x0007ffff));
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int32_t));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int16:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        *(reinterpret_cast<std::int16_t*>(interleavedBuffer) + (i * channelCount + j)) = (
-                            data[i] * static_cast<float>(0x00007fff));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int16RE:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        auto ptr = reinterpret_cast<std::int16_t*>(interleavedBuffer) + (i * channelCount + j);
-                        *ptr = (data[i] * static_cast<float>(0x00007fff));
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int16_t));
-                    }
-                    break;
-                }
-                case ALSABackend::SampleFormat::Int8:
-                {
-                    FOR_RANGE0(j, channelCount)
-                    {
-                        *(reinterpret_cast<std::int16_t*>(interleavedBuffer) + (i * channelCount + j)) = (
-                            data[i] * static_cast<float>(0x0000007f));
-                    }
-                    break;
-                }
-                default:
-                    break;
+                    item.doProcess();
                 }
             }
-        }
-        else
-        {
-            auto nonInterleavedArray = reinterpret_cast<std::byte**>(buffer.nonInterleavedBuffer);
-            FOR_RANGE0(j, channelCount)
-            {
-                auto nonInterleavedBuffer = nonInterleavedArray[j];
-                FOR_RANGE0(i, frameCount)
-                {
-                    switch(format)
-                    {
-                    case ALSABackend::SampleFormat::DoubleFloat:
-                    {
-                        *(reinterpret_cast<double*>(nonInterleavedBuffer) + i) = data[i];
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::DoubleFloatRE:
-                    {
-                        auto ptr = reinterpret_cast<double*>(nonInterleavedBuffer) + i;
-                        *ptr = data[i];
-                        YADAW::Util::reverseEndianness(ptr, sizeof(double));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Float:
-                    {
-                        *(reinterpret_cast<float*>(nonInterleavedBuffer) + i) = data[i];
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::FloatRE:
-                    {
-                        auto ptr = reinterpret_cast<float*>(nonInterleavedBuffer) + i;
-                        *ptr = data[i];
-                        YADAW::Util::reverseEndianness(ptr, sizeof(float));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int32:
-                    {
-                        *(reinterpret_cast<std::int32_t*>(nonInterleavedBuffer) + i) = (
-                            static_cast<double>(data[i]) * static_cast<double>(0x80000000) - data[i]);
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int32RE:
-                    {
-                        auto ptr = reinterpret_cast<std::int32_t*>(nonInterleavedBuffer) + i;
-                        *ptr = (static_cast<double>(data[i]) * static_cast<double>(0x80000000) - data[i]);
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int32_t));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int24:
-                    {
-                        *(reinterpret_cast<std::int32_t*>(nonInterleavedBuffer) + i) = (
-                            data[i] * static_cast<float>(0x007fffff));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int24RE:
-                    {
-                        auto ptr = reinterpret_cast<std::int32_t*>(nonInterleavedBuffer) + i;
-                        *ptr = (data[i] * static_cast<float>(0x007fffff));
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int32_t));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int20:
-                    {
-                        *(reinterpret_cast<std::int32_t*>(nonInterleavedBuffer) + i) = (
-                            data[i] * static_cast<float>(0x0007ffff));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int20RE:
-                    {
-                        auto ptr = reinterpret_cast<std::int32_t*>(nonInterleavedBuffer) + i;
-                        *ptr = (data[i] * static_cast<float>(0x0007ffff));
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int32_t));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int16:
-                    {
-                        *(reinterpret_cast<std::int16_t*>(nonInterleavedBuffer) + i) = (
-                            data[i] * static_cast<float>(0x00007fff));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int16RE:
-                    {
-                        auto ptr = reinterpret_cast<std::int16_t*>(nonInterleavedBuffer) + i;
-                        *ptr = (data[i] * static_cast<float>(0x00007fff));
-                        YADAW::Util::reverseEndianness(ptr, sizeof(std::int16_t));
-                        break;
-                    }
-                    case ALSABackend::SampleFormat::Int8:
-                    {
-                        *(reinterpret_cast<std::int16_t*>(nonInterleavedBuffer) + i) = (
-                            data[i] * static_cast<float>(0x0000007f));
-                        break;
-                    }
-                    default:
-                        break;
-                    }
-                }
-            }
-        }
+        );
     }
-    totalFrameCount += frameCount;
 }
