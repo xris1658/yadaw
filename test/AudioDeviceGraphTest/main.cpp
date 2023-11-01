5f2b7c4732f0fe0dba81e7cb32903e6b7c7edeed
xris1658 <chris1658@foxmail.com>
2023-11-01 21:05:15 +0800

    Add tests with summing device

--- test/AudioDeviceGraphTest/main.cpp
+++ test/AudioDeviceGraphTest/main.cpp
@@ -1,41 +1,72 @@
 #include "audio/engine/AudioDeviceGraph.hpp"
 #include "audio/engine/extension/UpstreamLatency.hpp"
 #include "audio/util/SampleDelay.hpp"
+#include "audio/util/Summing.hpp"
 #include "util/IntegerRange.hpp"
 
 #include <iostream>
 
 int main()
 {
-    YADAW::Audio::Engine::AudioDeviceGraph<
-        YADAW::Audio::Engine::Extension::UpstreamLatency> graph(512);
     std::vector<YADAW::Audio::Util::SampleDelay> sampleDelay;
     const auto nodeCount = 8;
     sampleDelay.reserve(nodeCount);
     std::vector<ade::NodeHandle> nodes;
     nodes.reserve(nodeCount);
+    std::vector<ade::EdgeHandle> edges;
+    edges.reserve(nodeCount - 1);
     YADAW::Audio::Util::AudioChannelGroup channelGroup;
     channelGroup.setChannelGroupType(YADAW::Audio::Base::ChannelGroupType::eStereo);
-    FOR_RANGE0(i, nodeCount)
+    YADAW::Audio::Util::Summing summing(nodeCount, channelGroup.type());
     {
-        sampleDelay.emplace_back((i * 5) % nodeCount + 1, channelGroup);
-        nodes.emplace_back(
-            graph.addNode(
-                YADAW::Audio::Engine::AudioDeviceProcess(sampleDelay[i])
-            )
-        );
-    }
-    FOR_RANGE0(i, nodeCount - 1)
-    {
-        graph.connect(nodes[i], nodes[i + 1], 0, 0);
-    }
-    auto& upstreamLatencyExt = graph.getExtension<YADAW::Audio::Engine::Extension::UpstreamLatency>();
-    for(const auto& node: nodes)
-    {
-        std::cout
-            << upstreamLatencyExt.getUpstreamLatency(node)
-            << ", "
-            << graph.getNodeData(node).process.device()->latencyInSamples()
-            << '\n';
+        YADAW::Audio::Engine::AudioDeviceGraph<
+            YADAW::Audio::Engine::Extension::UpstreamLatency> graph(512);
+        FOR_RANGE0(i, nodeCount)
+        {
+            sampleDelay.emplace_back((i * 5) % nodeCount + 1, channelGroup);
+            nodes.emplace_back(
+                graph.addNode(
+                    YADAW::Audio::Engine::AudioDeviceProcess(sampleDelay[i])
+                )
+            );
+        }
+        FOR_RANGE0(i, nodeCount - 1)
+        {
+            edges.emplace_back(*graph.connect(nodes[i], nodes[i + 1], 0, 0));
+        }
+        auto& upstreamLatencyExt = graph.getExtension<YADAW::Audio::Engine::Extension::UpstreamLatency>();
+        for(const auto& node: nodes)
+        {
+            std::cout
+                << upstreamLatencyExt.getUpstreamLatency(node)
+                << ", "
+                << graph.getNodeData(node).process.device()->latencyInSamples()
+                << '\n';
+        }
+        for(const auto& edge: edges)
+        {
+            graph.disconnect(edge);
+        }
+        for(const auto& node: nodes)
+        {
+            std::cout
+                << upstreamLatencyExt.getUpstreamLatency(node)
+                << ", "
+                << graph.getNodeData(node).process.device()->latencyInSamples()
+                << '\n';
+        }
+        auto summingNode = graph.addNode(YADAW::Audio::Engine::AudioDeviceProcess{summing});
+        edges.clear();
+        edges.reserve(nodeCount);
+        FOR_RANGE0(i, nodeCount)
+        {
+            edges.emplace_back(*(graph.connect(nodes[i], summingNode, 0, i)));
+            std::cout << upstreamLatencyExt.getUpstreamLatency(summingNode) << '\n';
+        }
+        FOR_RANGE0(i, nodeCount)
+        {
+            graph.disconnect(edges[(i + 1) * 3 % nodeCount]);
+            std::cout << upstreamLatencyExt.getUpstreamLatency(summingNode) << '\n';
+        }
     }
 }
