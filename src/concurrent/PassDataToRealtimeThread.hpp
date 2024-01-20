39607913a193c6f6a7759716acca419cec02fc9f
xris1658 <chris1658@foxmail.com>
2024-01-20 22:59:54 +0800

    Add facilities passing data to audio threads

---
+++ src/concurrent/PassDataToRealtimeThread.hpp
@@ -0,0 +1,51 @@
+#ifndef YADAW_SRC_CONCURRENT_PASSDATATOREALTIMETHREAD
+#define YADAW_SRC_CONCURRENT_PASSDATATOREALTIMETHREAD
+
+#include <array>
+#include <atomic>
+#include <memory>
+
+namespace YADAW::Concurrent
+{
+template<typename T>
+class PassDataToRealtimeThread
+{
+public:
+    PassDataToRealtimeThread(std::unique_ptr<T>&& initialData):
+        data_{std::move(initialData), std::unique_ptr<T>(nullptr)},
+        updated_(false)
+    {}
+public:
+    T* get()
+    {
+        return data_[0].get();
+    }
+    const T* get() const
+    {
+        return data_[0].get();
+    }
+    void update(std::unique_ptr<T>&& data)
+    {
+        data_[1] = std::move(data);
+        updated_.store(true, std::memory_order::memory_order_release);
+    }
+    void swapIfNeeded()
+    {
+        if(updated_.load(std::memory_order::memory_order_acquire))
+        {
+            std::swap(data_[0], data_[1]);
+            updated_.store(false, std::memory_order::memory_order_release);
+        }
+    }
+    void disposeOld()
+    {
+        while(updated_.load(std::memory_order::memory_order_acquire)) {}
+        data_[1].reset();
+    }
+private:
+    std::array<std::unique_ptr<T>, 2> data_;
+    std::atomic<bool> updated_;
+};
+}
+
+#endif // YADAW_SRC_CONCURRENT_PASSDATATOREALTIMETHREAD
---
+++ test/PassDataToRealtimeThreadTest/main.cpp
@@ -0,0 +1,41 @@
+#include "concurrent/PassDataToRealtimeThread.hpp"
+
+#include "common/DisableStreamBuffer.hpp"
+
+#include <thread>
+
+int main()
+{
+    disableStdOutBuffer();
+    YADAW::Concurrent::PassDataToRealtimeThread<int> passDataToRealtimeThread(
+        std::make_unique<int>(0)
+    );
+    std::thread realtimeThread(
+        [&passDataToRealtimeThread]()
+        {
+            while(true)
+            {
+                const auto& data = *(passDataToRealtimeThread.get());
+                // std::printf("%d\n", data);
+                if(data == -1)
+                {
+                    break;
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                passDataToRealtimeThread.swapIfNeeded();
+            }
+        }
+    );
+    while(true)
+    {
+        int newData;
+        std::scanf("%d", &newData);
+        passDataToRealtimeThread.update(std::make_unique<int>(newData));
+        passDataToRealtimeThread.disposeOld();
+        if(newData == -1)
+        {
+            break;
+        }
+    }
+    realtimeThread.join();
+}
