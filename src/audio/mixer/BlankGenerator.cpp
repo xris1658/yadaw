030f7e1ca39e60ee247fb23dd528db4e193b9ce5
xris1658 <chris1658@foxmail.com>
2024-01-24 09:42:00 +0800

    Add `BlankGenerator`

---
+++ src/audio/mixer/BlankGenerator.cpp
@@ -0,0 +1,53 @@
+#include "BlankGenerator.hpp"
+
+#include "util/IntegerRange.hpp"
+
+namespace YADAW::Audio::Mixer
+{
+BlankGenerator::BlankGenerator(
+    YADAW::Audio::Base::ChannelGroupType channelGroupType,
+    std::uint32_t channelCountInGroup):
+    output_()
+{
+    output_.setChannelGroupType(channelGroupType, channelCountInGroup);
+}
+
+std::uint32_t BlankGenerator::audioInputGroupCount() const
+{
+    return 0;
+}
+
+std::uint32_t BlankGenerator::audioOutputGroupCount() const
+{
+    return 1;
+}
+
+YADAW::Audio::Device::IAudioDevice::OptionalAudioChannelGroup
+    BlankGenerator::audioInputGroupAt(std::uint32_t index) const
+{
+    return std::nullopt;
+}
+
+YADAW::Audio::Device::IAudioDevice::OptionalAudioChannelGroup
+    BlankGenerator::audioOutputGroupAt(std::uint32_t index) const
+{
+    return index == 0?
+        OptionalAudioChannelGroup(std::ref(output_)):
+        std::nullopt;
+}
+
+std::uint32_t BlankGenerator::latencyInSamples() const
+{
+    return 0;
+}
+
+void BlankGenerator::process(
+    const Device::AudioProcessData<float>& audioProcessData)
+{
+    FOR_RANGE0(i, output_.channelCount())
+    {
+        std::memset(audioProcessData.outputs[0][i], 0,
+            sizeof(float) / sizeof(int) * audioProcessData.singleBufferSize);
+    }
+}
+}
---
+++ src/audio/mixer/BlankGenerator.hpp
@@ -0,0 +1,29 @@
+#ifndef YADAW_SRC_AUDIO_MIXER_BLANKGENERATOR
+#define YADAW_SRC_AUDIO_MIXER_BLANKGENERATOR
+
+#include "audio/device/IAudioDevice.hpp"
+#include "audio/util/AudioChannelGroup.hpp"
+
+namespace YADAW::Audio::Mixer
+{
+class BlankGenerator: public YADAW::Audio::Device::IAudioDevice
+{
+public:
+    BlankGenerator(YADAW::Audio::Base::ChannelGroupType channelGroupType,
+        std::uint32_t channelCountInGroup = 0);
+    BlankGenerator(const BlankGenerator&) = default;
+    BlankGenerator& operator=(const BlankGenerator&) = default;
+    ~BlankGenerator() noexcept = default;
+public:
+    std::uint32_t audioInputGroupCount() const override;
+    std::uint32_t audioOutputGroupCount() const override;
+    OptionalAudioChannelGroup audioInputGroupAt(std::uint32_t index) const override;
+    OptionalAudioChannelGroup audioOutputGroupAt(std::uint32_t index) const override;
+    std::uint32_t latencyInSamples() const override;
+    void process(const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData) override;
+private:
+    YADAW::Audio::Util::AudioChannelGroup output_;
+};
+}
+
+#endif // YADAW_SRC_AUDIO_MIXER_BLANKGENERATOR
