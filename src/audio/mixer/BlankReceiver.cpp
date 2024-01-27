8289d64f5edc8e56952743ae5e45f6ca7cbf7a63
xris1658 <chris1658@foxmail.com>
2024-01-27 10:19:27 +0800

    Add `BlankReceiver`

---
+++ src/audio/mixer/BlankReceiver.cpp
@@ -0,0 +1,58 @@
+#include "BlankReceiver.hpp"
+
+#include "util/IntegerRange.hpp"
+
+namespace YADAW::Audio::Mixer
+{
+BlankReceiver::BlankReceiver(
+    YADAW::Audio::Base::ChannelGroupType channelGroupType,
+    std::uint32_t channelCountInGroup):
+    input_()
+{
+    input_.setChannelGroupType(channelGroupType, channelCountInGroup);
+}
+
+std::uint32_t BlankReceiver::audioInputGroupCount() const
+{
+    return 1;
+}
+
+std::uint32_t BlankReceiver::audioOutputGroupCount() const
+{
+    return 0;
+}
+
+YADAW::Audio::Device::IAudioDevice::OptionalAudioChannelGroup
+BlankReceiver::audioInputGroupAt(std::uint32_t index) const
+{
+    return index == 0?
+        OptionalAudioChannelGroup(std::ref(input_)):
+        std::nullopt;
+}
+
+YADAW::Audio::Device::IAudioDevice::OptionalAudioChannelGroup
+BlankReceiver::audioOutputGroupAt(std::uint32_t index) const
+{
+    return std::nullopt;
+}
+
+std::uint32_t BlankReceiver::latencyInSamples() const
+{
+    return 0;
+}
+
+void BlankReceiver::process(
+    const Device::AudioProcessData<float>& audioProcessData)
+{}
+
+uint32_t BlankReceiver::audioChannelMapCount() const
+{
+    return 0;
+}
+
+YADAW::Audio::Device::IAudioDevice::OptionalChannelMap
+BlankReceiver::audioChannelMapAt(std::uint32_t index) const
+{
+    return std::nullopt;
+}
+}
---
+++ src/audio/mixer/BlankReceiver.hpp
@@ -0,0 +1,31 @@
+#ifndef YADAW_SRC_AUDIO_MIXER_BLANKRECEIVER
+#define YADAW_SRC_AUDIO_MIXER_BLANKRECEIVER
+
+#include "audio/device/IAudioDevice.hpp"
+#include "audio/util/AudioChannelGroup.hpp"
+
+namespace YADAW::Audio::Mixer
+{
+class BlankReceiver: public YADAW::Audio::Device::IAudioDevice
+{
+public:
+    BlankReceiver(YADAW::Audio::Base::ChannelGroupType channelGroupType,
+    std::uint32_t channelCountInGroup = 0);
+    BlankReceiver(const BlankReceiver&) = default;
+    BlankReceiver& operator=(const BlankReceiver&) = default;
+    ~BlankReceiver() noexcept = default;
+public:
+    std::uint32_t audioInputGroupCount() const override;
+    std::uint32_t audioOutputGroupCount() const override;
+    OptionalAudioChannelGroup audioInputGroupAt(std::uint32_t index) const override;
+    OptionalAudioChannelGroup audioOutputGroupAt(std::uint32_t index) const override;
+    std::uint32_t latencyInSamples() const override;
+    void process(const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData) override;
+    std::uint32_t audioChannelMapCount() const override;
+    OptionalChannelMap audioChannelMapAt(std::uint32_t index) const override;
+private:
+    YADAW::Audio::Util::AudioChannelGroup input_;
+};
+}
+
+#endif // YADAW_SRC_AUDIO_MIXER_BLANKRECEIVER
