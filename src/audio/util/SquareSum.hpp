6cec69988ef0999a3d019f4ffb74fd44e156d9f3
xris1658 <chris1658@foxmail.com>
2024-01-27 10:18:23 +0800

    Add `SquareSum`; fix commit ccd20ae0

---
+++ src/audio/util/SquareSum.hpp
@@ -0,0 +1,64 @@
+#ifndef YADAW_SRC_AUDIO_UTIL_SQUARESUM
+#define YADAW_SRC_AUDIO_UTIL_SQUARESUM
+
+#include <iterator>
+#include <numeric>
+
+namespace YADAW::Audio::Util
+{
+namespace Impl
+{
+template<typename T> struct SumSquare
+{
+    SumSquare(T value = T()): value_(value) {}
+    T value_;
+};
+
+template<typename T>
+T leftAddRightSquare(T lhs, T rhs)
+{
+    return lhs + rhs * rhs;
+}
+
+template<typename T>
+struct ReduceSumSquareHelper
+{
+    SumSquare<T> operator()(SumSquare<T> lhs, SumSquare<T> rhs)
+    {
+        return {lhs.value_ + rhs.value_};
+    }
+    SumSquare<T> operator()(SumSquare<T> lhs, T rhs)
+    {
+        return {lhs.value_ + rhs * rhs};
+    }
+    SumSquare<T> operator()(T lhs, SumSquare<T> rhs)
+    {
+        return {lhs * lhs + rhs.value_};
+    }
+    SumSquare<T> operator()(T lhs, T rhs)
+    {
+        return {lhs * lhs + rhs * rhs};
+    }
+};
+}
+
+template<
+    typename It,
+    bool Precise = false,
+    typename T = std::remove_const_t<typename std::iterator_traits<It>::value_type>
+>
+T squareSum(It first, It last)
+{
+    if constexpr(Precise)
+    {
+        return std::accumulate(first, last, &Impl::leftAddRightSquare<T>);
+    }
+    else
+    {
+        return std::reduce(first, last, Impl::SumSquare<T>(),
+            Impl::ReduceSumSquareHelper<T>()).value_;
+    }
+}
+}
+
+#endif // YADAW_SRC_AUDIO_UTIL_SQUARESUM
