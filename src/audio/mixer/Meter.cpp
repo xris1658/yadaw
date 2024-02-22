2569a9c371c53b91bf40ca49ce0fc154466f2a47
xris1658 <chris1658@foxmail.com>
2024-02-22 22:02:42 +0800

    Implement peak and RMS methods in `Meter`; match commit 82e26d81

--- src/audio/mixer/Meter.cpp
+++ src/audio/mixer/Meter.cpp
@@ -17,9 +17,9 @@ Meter::Meter(
     rmsWindowSize_(rmsWindowSize)
 {
     input_.setChannelGroupType(channelGroupType, channelCountInGroup);
+    output_.setChannelGroupType(channelGroupType, channelCountInGroup);
     auto channelCount = input_.channelCount();
     currentPeaks_.resize(channelCount, 0.0f);
-    highestPeaks_.resize(channelCount, 0.0f);
     rms_.resize(channelCount, 0.0f);
     samples_.resize(channelCount,
         std::vector<float>(rmsWindowSize, 0.0f)
@@ -33,7 +33,7 @@ std::uint32_t Meter::audioInputGroupCount() const
 
 std::uint32_t Meter::audioOutputGroupCount() const
 {
-    return 0;
+    return 1;
 }
 
 YADAW::Audio::Device::IAudioDevice::OptionalAudioChannelGroup
@@ -47,7 +47,9 @@ YADAW::Audio::Device::IAudioDevice::OptionalAudioChannelGroup
 YADAW::Audio::Device::IAudioDevice::OptionalAudioChannelGroup
     Meter::audioOutputGroupAt(std::uint32_t index) const
 {
-    return std::nullopt;
+    return index == 0?
+        OptionalAudioChannelGroup(std::ref(output_)):
+        std::nullopt;
 }
 
 std::uint32_t Meter::latencyInSamples() const
@@ -58,73 +60,125 @@ std::uint32_t Meter::latencyInSamples() const
 void Meter::process(const Device::AudioProcessData<float>& audioProcessData)
 {
     const auto bufferSize = audioProcessData.singleBufferSize;
-    if(bufferSize >= rmsWindowSize_)
+    if(accessingPeak_.try_lock())
     {
         FOR_RANGE0(i, input_.channelCount())
         {
-            std::memcpy(samples_[i].data(),
-                audioProcessData.inputs[0][i] + (bufferSize - rmsWindowSize_),
-                sizeof(bufferSize) * sizeof(float)
+            currentPeaks_[i] = std::abs(
+                *std::max_element(
+                    audioProcessData.inputs[0][i],
+                    audioProcessData.inputs[0][i] + bufferSize,
+                    &compareAbs
+                )
             );
         }
-        index_ = 0;
+        accessingPeak_.unlock();
     }
-    else
+    if(accessingRMS_.try_lock())
     {
-        auto oldIndex = index_;
-        index_ += bufferSize;
-        if(index_ >= rmsWindowSize_)
+        if(bufferSize >= rmsWindowSize_)
         {
             FOR_RANGE0(i, input_.channelCount())
             {
-                std::memcpy(samples_[i].data() + oldIndex,
-                    audioProcessData.inputs[0][i],
-                    (rmsWindowSize_ - oldIndex) * sizeof(float)
-                );
                 std::memcpy(samples_[i].data(),
-                    audioProcessData.inputs[0][i] + (rmsWindowSize_ - oldIndex),
-                    (index_ - rmsWindowSize_) * sizeof(float)
+                    audioProcessData.inputs[0][i] + (bufferSize - rmsWindowSize_),
+                    sizeof(bufferSize) * sizeof(float)
                 );
             }
-            index_ -= rmsWindowSize_;
+            index_ = 0;
         }
         else
         {
-            FOR_RANGE0(i, input_.channelCount())
+            auto oldIndex = index_;
+            index_ += bufferSize;
+            if(index_ >= rmsWindowSize_)
             {
-                std::memcpy(samples_[i].data() + oldIndex,
-                    audioProcessData.inputs[0][i],
-                    bufferSize * sizeof(float)
-                );
+                FOR_RANGE0(i, input_.channelCount())
+                {
+                    std::memcpy(samples_[i].data() + oldIndex,
+                        audioProcessData.inputs[0][i],
+                        (rmsWindowSize_ - oldIndex) * sizeof(float)
+                    );
+                    std::memcpy(samples_[i].data(),
+                        audioProcessData.inputs[0][i] + (rmsWindowSize_ - oldIndex),
+                        (index_ - rmsWindowSize_) * sizeof(float)
+                    );
+                }
+                index_ -= rmsWindowSize_;
             }
+            else
+            {
+                FOR_RANGE0(i, input_.channelCount())
+                {
+                    std::memcpy(samples_[i].data() + oldIndex,
+                        audioProcessData.inputs[0][i],
+                        bufferSize * sizeof(float)
+                    );
+                }
 
+            }
         }
+        FOR_RANGE0(i, input_.channelCount())
+        {
+            rms_[i] = std::sqrt(
+                YADAW::Audio::Util::squareSum(
+                    samples_[i].begin(), samples_[i].end()
+                )
+            );
+        }
+        accessingRMS_.unlock();
     }
-    FOR_RANGE0(i, input_.channelCount())
+}
+
+std::pair<const float*, std::unique_lock<YADAW::Util::AtomicMutex>> Meter::currentPeaks() const
+{
+    return {
+        currentPeaks_.data(),
+        std::unique_lock<YADAW::Util::AtomicMutex>(accessingPeak_)
+    };
+}
+
+void Meter::resetPeak()
+{
+    std::unique_lock<YADAW::Util::AtomicMutex> lock(accessingPeak_);
+    std::fill(currentPeaks_.begin(), currentPeaks_.end(), 0.0f);
+}
+
+std::uint32_t Meter::rmsWindowSize() const
+{
+    return rmsWindowSize_;
+}
+
+std::pair<const float*, std::unique_lock<YADAW::Util::AtomicMutex>> Meter::currentRMS() const
+{
+    return {
+        rms_.data(),
+        std::unique_lock<YADAW::Util::AtomicMutex>(accessingRMS_)
+    };
+}
+
+void Meter::setRMSWindowSize(std::uint32_t rmsWindowSize)
+{
+    std::unique_lock<YADAW::Util::AtomicMutex> lock(accessingRMS_);
+    FOR_RANGE0(i, samples_.size())
     {
-        currentPeaks_[i] = std::abs(
-            *std::max_element(
-                audioProcessData.inputs[0][i],
-                audioProcessData.inputs[0][i] + bufferSize,
-                &compareAbs
-            )
-        );
-        rms_[i] = std::sqrt(
-            YADAW::Audio::Util::squareSum(
-                samples_[i].begin(), samples_[i].end()
-            )
-        );
+        samples_[i].clear();
+        samples_[i].shrink_to_fit();
+        samples_[i].resize(rmsWindowSize, 0.0f);
     }
+    for(auto& rms: rms_)
+    {
+        rms = 0.0f;
+    }
+    index_ = 0;
 }
 
-std::uint32_t Meter::audioChannelMapCount() const
+void Meter::resetRMS()
 {
-    return 0;
-}
-
-YADAW::Audio::Device::IAudioDevice::OptionalChannelMap
-    Meter::audioChannelMapAt(std::uint32_t index) const
-{
-    return std::nullopt;
+    std::unique_lock<YADAW::Util::AtomicMutex> lock(accessingRMS_);
+    for(auto& rms: rms_)
+    {
+        rms = 0.0f;
+    }
 }
 }
--- src/audio/mixer/Meter.hpp
+++ src/audio/mixer/Meter.hpp
@@ -3,6 +3,7 @@
 
 #include "audio/device/IAudioDevice.hpp"
 #include "audio/util/AudioChannelGroup.hpp"
+#include "util/AtomicMutex.hpp"
 
 namespace YADAW::Audio::Mixer
 {
@@ -19,23 +20,28 @@ public:
     OptionalAudioChannelGroup audioOutputGroupAt(std::uint32_t index) const override;
     std::uint32_t latencyInSamples() const override;
     void process(const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData) override;
-    std::uint32_t audioChannelMapCount() const override;
-    OptionalChannelMap audioChannelMapAt(std::uint32_t index) const override;
 public:
-    float currentPeak(std::uint32_t index) const;
-    float currentPeakAt(std::uint32_t index) const;
+    std::pair<
+        const float*,
+        std::unique_lock<YADAW::Util::AtomicMutex>
+    > currentPeaks() const;
+    void resetPeak();
     std::uint32_t rmsWindowSize() const;
-    bool setRMSWindowSize(std::uint32_t rmsWindowSize);
-    float rms(std::uint32_t index) const;
-    float rmsAt(std::uint32_t index) const;
+    std::pair<
+        const float*,
+        std::unique_lock<YADAW::Util::AtomicMutex>
+    > currentRMS() const;
+    void setRMSWindowSize(std::uint32_t rmsWindowSize);
     void resetRMS();
 private:
     YADAW::Audio::Util::AudioChannelGroup input_;
+    YADAW::Audio::Util::AudioChannelGroup output_;
     std::vector<float> currentPeaks_;
-    std::vector<float> highestPeaks_;
     std::vector<float> rms_;
     std::uint32_t rmsWindowSize_;
     std::uint32_t index_ = 0;
+    mutable YADAW::Util::AtomicMutex accessingPeak_;
+    mutable YADAW::Util::AtomicMutex accessingRMS_;
     std::vector<std::vector<float>> samples_;
 };
 }
