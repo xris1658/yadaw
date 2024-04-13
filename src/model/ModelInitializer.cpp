9a1df169398f18ca434f410a6421610907b285cf
xris1658 <chris1658@foxmail.com>
2024-04-13 17:47:58 +0800

    Register model interfaces as uncreatable; prevent compilation errors on Qt >= 6.6

--- src/model/ModelInitializer.cpp
+++ src/model/ModelInitializer.cpp
@@ -26,26 +26,28 @@
 
 namespace YADAW::Model
 {
+QString modelIsInterfaceText = "This model is an abstract model instantiated in C++ codes.";
+
 void initializeModel()
 {
-    QML_REGISTER_TYPE(IAssetDirectoryListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IAudioBusChannelListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IAudioBusConfigurationModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IAudioDeviceIOGroupListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IAudioDeviceListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IAutomationModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IFileTreeModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(ILocalizationListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IMixerChannelInsertListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IMixerChannelListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IParameterValueAndTextListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IPluginCategoryListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IPluginDirectoryListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IPluginListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(IPluginParameterListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(ISortFilterListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(ISpeakerListModel, YADAW.Models, 1, 0);
-    QML_REGISTER_TYPE(ITrackListModel, YADAW.Models, 1, 0);
+    QML_REGISTER_UNCREATABLE_TYPE(IAssetDirectoryListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IAudioBusChannelListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IAudioBusConfigurationModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IAudioDeviceIOGroupListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IAudioDeviceListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IAutomationModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IFileTreeModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(ILocalizationListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IMixerChannelInsertListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IMixerChannelListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IParameterValueAndTextListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IPluginCategoryListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IPluginDirectoryListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IPluginListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(IPluginParameterListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(ISortFilterListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(ISpeakerListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
+    QML_REGISTER_UNCREATABLE_TYPE(ITrackListModel, YADAW.Models, 1, 0, modelIsInterfaceText);
 
     QML_REGISTER_TYPE(FilterRoleModel, YADAW.Models, 1, 0);
     QML_REGISTER_TYPE(SortFilterProxyListModel, YADAW.Models, 1, 0);
--- src/util/QmlUtil.hpp
+++ src/util/QmlUtil.hpp
@@ -6,4 +6,7 @@
 #define QML_REGISTER_TYPE(class_name, module_name, major_version, minor_version) \
     qmlRegisterType<class_name>(#module_name, major_version, minor_version, #class_name)
 
+#define QML_REGISTER_UNCREATABLE_TYPE(class_name, module_name, major_version, minor_version, reason) \
+    qmlRegisterUncreatableType<class_name>(#module_name, major_version, minor_version, #class_name, reason)
+
 #endif // YADAW_SRC_UTIL_QMLUTIL
