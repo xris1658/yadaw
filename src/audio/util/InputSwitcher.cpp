edd3be7fc06a3e53a28e0e15ff2735822502b322
xris1658 <chris1658@foxmail.com>
2024-07-24 09:03:34 +0800

    Use SIMD instructions in `InputSwitcher`

--- src/audio/util/InputSwitcher.cpp
+++ src/audio/util/InputSwitcher.cpp
@@ -4,6 +4,8 @@
 
 #include <stdexcept>
 
+#include <xmmintrin.h>
+
 namespace YADAW::Audio::Util
 {
 InputSwitcher::InputSwitcher(std::uint32_t inputCount,
@@ -52,10 +54,20 @@ std::uint32_t InputSwitcher::latencyInSamples() const
     return 0U;
 }
 
-void InputSwitcher::process(
-    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData)
+using ProcessFunc = void(
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData,
+    std::uint32_t inputIndex);
+
+template<bool Aligned>
+void doProcess(
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData,
+    std::uint32_t inputIndex);
+
+template<>
+void doProcess<false>(
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData,
+    std::uint32_t inputIndex)
 {
-    auto inputIndex = inputIndex_.load(std::memory_order_relaxed);
     FOR_RANGE0(i, audioProcessData.outputCounts[0])
     {
         std::memcpy(
@@ -64,6 +76,41 @@ void InputSwitcher::process(
             audioProcessData.singleBufferSize * sizeof(float)
         );
     }
+}
+
+template<>
+void doProcess<true>(
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData,
+    std::uint32_t inputIndex)
+{
+    FOR_RANGE0(i, audioProcessData.outputCounts[0])
+    {
+        auto alignCount = audioProcessData.singleBufferSize * sizeof(float) / sizeof(__m128);
+        auto alignedInput = reinterpret_cast<__m128*>(audioProcessData.inputs[inputIndex][i]);
+        auto output = audioProcessData.outputs[0][i];
+        FOR_RANGE0(j, alignCount)
+        {
+            _mm_store_ps(
+                output + j * sizeof(__m128) / sizeof(float),
+                alignedInput[j]
+            );
+        }
+    }
+}
+
+ProcessFunc* const processFuncs[2] = {
+    &doProcess<false>,
+    &doProcess<true>,
+};
+
+void InputSwitcher::process(
+    const YADAW::Audio::Device::AudioProcessData<float>& audioProcessData)
+{
+    auto inputIndex = inputIndex_.load(std::memory_order_relaxed);
+    bool aligned =
+        (reinterpret_cast<std::uint64_t>(audioProcessData.outputs[0][0]) & 0x0000000F) == 0
+     && (reinterpret_cast<std::uint64_t>(audioProcessData.inputs [inputIndex][0]) & 0x0000000F) == 0;
+    processFuncs[aligned](audioProcessData, inputIndex);
 }
 
 std::uint32_t InputSwitcher::getInputIndex() const
