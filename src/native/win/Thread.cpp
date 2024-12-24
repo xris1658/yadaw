a3b6b90e7be949c450588d785367ba5e38e44434
xris1658 <chris1658@foxmail.com>
2024-12-24 21:21:41 +0800

    Implement thread affinity functions on Windows

---
+++ src/native/win/Thread.cpp
@@ -0,0 +1,15 @@
+#if _WIN32
+
+#include "native/Thread.hpp"
+
+#include <windows.h>
+
+namespace YADAW::Native
+{
+bool setThreadAffinity(std::thread::native_handle_type handle, std::uint64_t affinity)
+{
+    return SetThreadAffinityMask(handle, affinity);
+}
+}
+
+#endif
