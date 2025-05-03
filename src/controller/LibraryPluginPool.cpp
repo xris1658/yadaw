313f9277986212ecee1dc228315e24df8ae2ae71
xris1658 <chris1658@foxmail.com>
2025-05-03 10:59:35 +0800

    Add `LibraryPluginPool`

---
+++ src/controller/LibraryPluginPool.cpp
@@ -0,0 +1,123 @@
+#include "LibraryPluginPool.hpp"
+
+namespace YADAW::Controller
+{
+LibraryPluginPool::Instance::Instance():
+    pool_(nullptr)
+{}
+
+LibraryPluginPool::Instance::Instance(
+    LibraryPluginPool& pool,
+    LibraryPluginPool::Pool::iterator libraryIt,
+    LibraryPluginPool::Pool::value_type::second_type::iterator pluginIt):
+    pool_(&pool)
+{
+    new(YADAW::Util::AlignHelper<LibraryIterator>::fromAligned(&libraryIt_)) LibraryIterator(libraryIt);
+    new(YADAW::Util::AlignHelper<PluginIterator>::fromAligned(&pluginIt_)) PluginIterator(pluginIt);
+}
+
+LibraryPluginPool::Instance::Instance(Instance&& rhs):
+    pool_(rhs.pool_)
+{
+    new(YADAW::Util::AlignHelper<LibraryIterator>::fromAligned(&libraryIt_)) LibraryIterator(
+        *YADAW::Util::AlignHelper<LibraryIterator>::fromAligned(&rhs.libraryIt_)
+    );
+    new(YADAW::Util::AlignHelper<PluginIterator>::fromAligned(&pluginIt_)) PluginIterator(
+        *YADAW::Util::AlignHelper<PluginIterator>::fromAligned(&rhs.pluginIt_)
+    );
+    pool_ = std::exchange(rhs.pool_, nullptr);
+    std::destroy_at(YADAW::Util::AlignHelper<LibraryIterator>::fromAligned(&rhs.libraryIt_));
+    std::destroy_at(YADAW::Util::AlignHelper<PluginIterator>::fromAligned(&rhs.pluginIt_));
+}
+
+LibraryPluginPool::Instance::~Instance()
+{
+    if(pool_)
+    {
+        auto libraryIt = YADAW::Util::AlignHelper<LibraryIterator>::fromAligned(&libraryIt_);
+        auto pluginIt = YADAW::Util::AlignHelper<PluginIterator>::fromAligned(&pluginIt_);
+        (*libraryIt)->second.erase(*pluginIt);
+        if((*libraryIt)->second.empty() && pool_->unloadLibraryIfNoPlugins())
+        {
+            pool_->pool_.erase(*libraryIt);
+        }
+        std::destroy_at(libraryIt);
+        std::destroy_at(pluginIt);
+    }
+}
+
+LibraryPluginPool::Instance::operator bool() const
+{
+    return pool_;
+}
+
+OptionalRef<const YADAW::Native::Library> LibraryPluginPool::Instance::library() const
+{
+    if(pool_)
+    {
+        return {(*YADAW::Util::AlignHelper<LibraryIterator>::fromAligned(&libraryIt_))->first};
+    }
+    return std::nullopt;
+}
+
+OptionalRef<YADAW::Audio::Plugin::IAudioPlugin> LibraryPluginPool::Instance::plugin() const
+{
+    if(pool_)
+    {
+        return {***YADAW::Util::AlignHelper<PluginIterator>::fromAligned(&pluginIt_)};
+    }
+    return std::nullopt;
+}
+
+bool LibraryPluginPool::unloadLibraryIfNoPlugins() const
+{
+    return unloadLibraryIfNoPlugins_;
+}
+
+void LibraryPluginPool::setUnloadLibraryIfNoPlugins(bool unloadLibraryIfNoPlugins)
+{
+    if(unloadLibraryIfNoPlugins ^ unloadLibraryIfNoPlugins_)
+    {
+        unloadLibraryIfNoPlugins_ = unloadLibraryIfNoPlugins;
+        if(unloadLibraryIfNoPlugins)
+        {
+            std::erase_if(
+                pool_,
+                [](Pool::const_reference pair)
+                {
+                    return pair.second.empty();
+                }
+            );
+        }
+    }
+}
+
+LibraryPluginPool::Instance LibraryPluginPool::createPluginInstance(
+    const QString& path, std::function<CreatePluginFromLibraryCallback>& callback)
+{
+    auto it = pool_.lower_bound(path);
+    if(it == pool_.end() || it->first.path() != path)
+    {
+        if(YADAW::Native::Library library(path); library.loaded())
+        {
+            it = pool_.emplace_hint(
+                it,
+                std::make_pair(std::move(library), Pool::value_type::second_type())
+            );
+        }
+        else
+        {
+            return {};
+        }
+    }
+    if(auto plugin = callback(it->first))
+    {
+        return {*this, it, it->second.emplace(std::move(plugin)).first};
+    }
+    if(it->second.empty() && unloadLibraryIfNoPlugins_)
+    {
+        pool_.erase(it);
+    }
+    return {};
+}
+}
---
+++ src/controller/LibraryPluginPool.hpp
@@ -0,0 +1,113 @@
+#ifndef YADAW_SRC_CONTROLLER_LIBRARYPLUGINPOOL
+#define YADAW_SRC_CONTROLLER_LIBRARYPLUGINPOOL
+
+#include "audio/plugin/IAudioPlugin.hpp"
+#include "native/Library.hpp"
+#include "util/AlignHelper.hpp"
+
+#include <map>
+#include <memory>
+#include <set>
+
+namespace YADAW::Controller
+{
+namespace Impl
+{
+using YADAW::Native::Library;
+struct CompareLibrary
+{
+    using is_transparent = void;
+    bool operator()(const Library& lhs, const Library& rhs) const
+    {
+        return lhs.path() < rhs.path();
+    }
+    bool operator()(const Library& lhs, const QString& rhs) const
+    {
+        return lhs.path() < rhs;
+    }
+    bool operator()(const QString& lhs, const Library& rhs) const
+    {
+        return lhs < rhs.path();
+    }
+};
+
+struct ComparePlugin
+{
+    using is_transparent = void;
+    bool operator()(
+        const std::unique_ptr<YADAW::Audio::Plugin::IAudioPlugin>& lhs,
+        const std::unique_ptr<YADAW::Audio::Plugin::IAudioPlugin>& rhs) const
+    {
+        return lhs < rhs;
+    }
+    bool operator()(
+        const std::unique_ptr<YADAW::Audio::Plugin::IAudioPlugin>& lhs,
+        const YADAW::Audio::Plugin::IAudioPlugin* rhs) const
+    {
+        return lhs.get() < rhs;
+    }
+    bool operator()(
+        const YADAW::Audio::Plugin::IAudioPlugin* lhs,
+        const std::unique_ptr<YADAW::Audio::Plugin::IAudioPlugin>& rhs) const
+    {
+        return lhs < rhs.get();
+    }
+};
+}
+
+class LibraryPluginPool
+{
+    using Pool = std::map<
+        YADAW::Native::Library,
+        std::set<
+            std::unique_ptr<YADAW::Audio::Plugin::IAudioPlugin>,
+            Impl::ComparePlugin
+        >,
+        Impl::CompareLibrary
+    >;
+    using LibraryIterator = Pool::iterator;
+    using PluginIterator = Pool::value_type::second_type::iterator;
+    friend class Instance;
+public:
+    using CreatePluginFromLibraryCallback =
+    std::unique_ptr<YADAW::Audio::Plugin::IAudioPlugin>(
+        const YADAW::Native::Library& library
+    );
+public:
+    class Instance
+    {
+        friend class LibraryPluginPool;
+    public:
+        Instance();
+    private:
+        Instance(
+            LibraryPluginPool& pool,
+            LibraryPluginPool::LibraryIterator libraryIt,
+            LibraryPluginPool::PluginIterator pluginIt
+        );
+    public:
+        Instance(Instance&& rhs);
+        ~Instance();
+    public:
+        operator bool() const;
+        OptionalRef<const YADAW::Native::Library> library() const;
+        OptionalRef<YADAW::Audio::Plugin::IAudioPlugin> plugin() const;
+    private:
+        LibraryPluginPool* pool_;
+        YADAW::Util::AlignedStorage<LibraryPluginPool::LibraryIterator> libraryIt_;
+        YADAW::Util::AlignedStorage<LibraryPluginPool::PluginIterator> pluginIt_;
+    };
+public:
+    bool unloadLibraryIfNoPlugins() const;
+    void setUnloadLibraryIfNoPlugins(bool unloadLibraryIfNoPlugins);
+    Instance createPluginInstance(
+        const QString& path,
+        std::function<CreatePluginFromLibraryCallback>& callback
+    );
+private:
+    Pool pool_;
+    bool unloadLibraryIfNoPlugins_;
+};
+}
+
+#endif // YADAW_SRC_CONTROLLER_LIBRARYPLUGINPOOL
