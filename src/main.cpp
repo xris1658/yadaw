06d5c361131aad50258b2a1325d94e5a78570f04
xris1658 <chris1658@foxmail.com>
2025-07-13 08:47:21 +0800

    Add notes about D3D11 flip mode swap chain

--- src/main.cpp
+++ src/main.cpp
@@ -69,25 +69,32 @@ int main(int argc, char *argv[])
     // A temporary workaround to solve/avoid GUI performance issues on Windows
     // while dragging the window.
     // On Windows, Direct3D 11 is the default backend of the scene graph
-    // threaded renderer. When the D3D11 swap chain is created in flip mode (
-    // with `DXGI_SWAP_EFFECT_FLIP_xxx` as swap effect), swapping frames takes
-    // too much time (about 50ms per swap operation) when dragging a window,
-    // causing visible stutters.
-    // An enviroment variable is added in Qt 6.6.1 to choose if the backend
-    // creates the swap chain in flip mode. It's undocumented, so it might not
-    // work in future versions of Qt.
-    // (See https://github.com/qt/qtbase/blob/e2cbce919ccefcae2b18f90257d67bc6e24c3c94/src/gui/rhi/qrhid3d11.cpp#L198)
+    // threaded renderer. When the D3D11 swap chain is created in flip mode
+    // (i.e. with `DXGI_SWAP_EFFECT_FLIP_xxx` as swap effect), swapping frames
+    // takes too much time (about 50ms per swap operation under 60Hz, which is
+    // about 3 frames) when dragging a window, causing visible stutters (you can
+    // see it in other Qt applications that uses Direct3D 11, like this one:
+    // https://github.com/giladreich/QtDirect3D/issues/10).
+    // The enviroment variable `QT_D3D_NO_FLIP` is added BACK* in Qt 6.6.1 to
+    // choose if the backend creates the swap chain in flip mode. Since it's
+    // undocumented, it might not work in future versions of Qt.
+    // Note that bitblt swap chain cannot be created if the flag contains
+    // `DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING`, which is used if vsync is disabled
+    // (by setting `QSG_NO_VSYNC` to 1).
+    // (* This variable was removed in Qt 6.5.0: https://github.com/qt/qtbase/commit/3ee65daa74f36434b5a3d78b509247511d7867e1
+    // And this commit added it back: https://github.com/qt/qtbase/commit/bd76a9a86fbca0b646c59b9a2796d63cdac8373c
+    // )
     if(QLibraryInfo::version() >= QVersionNumber(6, 6, 1))
     {
         qputenv("QT_D3D_NO_FLIP", "1");
     }
     // As for older Qt versions before Qt 6.6.1, I just avoid using Direct3D by
-    // switching to software rendering. The main window UI is not that dynamic
-    // and complicated for now, so the performance is still okay.
+    // switching to software rendering.
     // TODO: Control the rendering process by using `QQuickRenderControl`
     // I don't use OpenGL or Vulkan as the backend because visible artifacts
-    // (black screen, Windows 7 classic title bar) can be seen while entering or
-    // leaving full screen mode.
+    // (black screen, Windows 7 classic title bar) occur for about 1s while
+    // entering or leaving full screen mode, which is even LONGER than software
+    // rendering.
     else
     {
         qputenv("QSG_NO_VSYNC", "1");
