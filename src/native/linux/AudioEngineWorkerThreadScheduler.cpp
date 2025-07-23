b9420ac38764fed9bb1f8e194b2db98cec615c26
xris1658 <chris1658@foxmail.com>
2025-07-23 14:34:27 +0800

    Set audio worker thread schedule params on Linux

--- src/native/linux/AudioEngineWorkerThreadScheduler.cpp
+++ src/native/linux/AudioEngineWorkerThreadScheduler.cpp
@@ -2,18 +2,25 @@
 
 #include "native/AudioEngineWorkerThreadScheduler.hpp"
 
+#include "native/Thread.hpp"
+
+#include <sched.h>
+
 namespace YADAW::Audio::Engine
 {
 AudioEngineWorkerThreadPool::ThreadScheduler::ThreadScheduler(
     const AudioEngineWorkerThreadPool& sender, const InitArgs& initArgs)
 {
-    // TODO
+    auto handle = YADAW::Native::getCurrentThreadHandle();
+    int policy;
+    sched_param schedParam;
+    pthread_getschedparam(handle, &policy, &schedParam);
+    schedParam.sched_priority = 20;
+    pthread_setschedparam(handle, SCHED_FIFO, &schedParam); // TODO: Use `SCHED_DEADLINE`?
 }
 
 AudioEngineWorkerThreadPool::ThreadScheduler::~ThreadScheduler()
-{
-    // TODO
-}
+{}
 }
 
 #endif
