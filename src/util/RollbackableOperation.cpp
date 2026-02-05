cb295dcea71c057cd13014276742e6451c67c3c4
xris1658 <chris1658@foxmail.com>
2026-02-05 18:24:26 +0800

    Move definition of `RollbackablePromise` to header

--- src/util/RollbackableOperation.cpp
+++ src/util/RollbackableOperation.cpp
@@ -2,30 +2,6 @@
 
 namespace YADAW::Util
 {
-struct RollbackablePromise
-{
-    RollbackableOperation get_return_object()
-    {
-        return {RollbackableOperation::from_promise(*this)};
-    }
-    std::suspend_never initial_suspend() noexcept
-    {
-        return {};
-    }
-    std::suspend_always final_suspend() noexcept
-    {
-        return {};
-    }
-    std::suspend_always yield_value(bool& shouldCommit) noexcept
-    {
-        shouldCommit_ = std::reference_wrapper<bool>(shouldCommit);
-        return {};
-    }
-    void return_void() {}
-    void unhandled_exception() {}
-    OptionalRef<bool> shouldCommit_ = std::nullopt;
-};
-
 void RollbackableOperation::resume(bool shouldCommit)
 {
     if(auto optRefShouldCommit = promise().shouldCommit_)
--- src/util/RollbackableOperation.hpp
+++ src/util/RollbackableOperation.hpp
@@ -66,6 +66,29 @@ public:
     void rollback() { resume(false); }
     void resume(bool shouldCommit);
 };
+struct RollbackablePromise
+{
+    RollbackableOperation get_return_object()
+    {
+        return {RollbackableOperation::from_promise(*this)};
+    }
+    std::suspend_never initial_suspend() noexcept
+    {
+        return {};
+    }
+    std::suspend_always final_suspend() noexcept
+    {
+        return {};
+    }
+    std::suspend_always yield_value(bool& shouldCommit) noexcept
+    {
+        shouldCommit_ = std::reference_wrapper<bool>(shouldCommit);
+        return {};
+    }
+    void return_void() {}
+    void unhandled_exception() {}
+    OptionalRef<bool> shouldCommit_ = std::nullopt;
+};
 }
 
 #endif // YADAW_SRC_UTIL_ROLLBACKABLEOPERATION
