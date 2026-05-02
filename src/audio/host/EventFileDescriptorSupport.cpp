2fec673cf393e0b4bccc43d78c47949b794fc00f
xris1658 <chris1658@foxmail.com>
2026-05-02 13:28:40 +0800

    Fix CLAP event flag conversion; fix 6487c8ee

--- src/audio/host/EventFileDescriptorSupport.cpp
+++ src/audio/host/EventFileDescriptorSupport.cpp
@@ -15,23 +15,23 @@ namespace YADAW::Audio::Host
 {
 std::array<epoll_event, 64> epollEvents;
 
-std::uint32_t eventFlagsFromCLAP(clap_posix_fd_flags_t flags)
+constexpr std::uint32_t eventFlagsFromCLAP(clap_posix_fd_flags_t flags)
 {
-    std::uint32_t ret = 0U;
-    if(flags & CLAP_POSIX_FD_READ)
-    {
-        flags |= EPOLL_EVENTS::EPOLLIN;
-    }
-    if(flags & CLAP_POSIX_FD_WRITE)
-    {
-        flags |= EPOLL_EVENTS::EPOLLOUT;
-    }
-    if(flags & CLAP_POSIX_FD_ERROR)
-    {
-        flags |= EPOLL_EVENTS::EPOLLERR;
-    }
-    return ret;
+    return
+        ((flags & CLAP_POSIX_FD_READ)?  EPOLLIN:  0) +
+        ((flags & CLAP_POSIX_FD_WRITE)? EPOLLOUT: 0) +
+        ((flags & CLAP_POSIX_FD_ERROR)? EPOLLERR: 0);
 }
+
+static_assert(
+    eventFlagsFromCLAP(CLAP_POSIX_FD_READ)  == EPOLLIN  &&
+    eventFlagsFromCLAP(CLAP_POSIX_FD_WRITE) == EPOLLOUT &&
+    eventFlagsFromCLAP(CLAP_POSIX_FD_ERROR) == EPOLLERR &&
+    eventFlagsFromCLAP(CLAP_POSIX_FD_READ | CLAP_POSIX_FD_WRITE) == (EPOLLIN | EPOLLOUT) &&
+    eventFlagsFromCLAP(CLAP_POSIX_FD_READ | CLAP_POSIX_FD_ERROR) == (EPOLLIN | EPOLLERR) &&
+    eventFlagsFromCLAP(CLAP_POSIX_FD_WRITE | CLAP_POSIX_FD_ERROR) == (EPOLLOUT | EPOLLERR) &&
+    eventFlagsFromCLAP(CLAP_POSIX_FD_READ | CLAP_POSIX_FD_WRITE | CLAP_POSIX_FD_ERROR) == (EPOLLIN | EPOLLOUT | EPOLLERR)
+);
 
 EventFileDescriptorSupport::EventFileDescriptorSupport():
     epollFD_(epoll_create1(0))
