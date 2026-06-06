4420541bd210ded07328d996bab3d1e6d47ffdf4
xris1658 <chris1658@foxmail.com>
2026-06-06 13:11:21 +0800

    Add debug messages on Windows

--- src/ui/ResizeEventFilter.cpp
+++ src/ui/ResizeEventFilter.cpp
@@ -18,7 +18,174 @@
 #endif
 
 #if _WIN32
+#if YADAW_DEBUG_RESIZE_EVENT_FILTER
+std::map<UINT, const char*> messages {
+    {WM_NULL, "WM_NULL"},
+    {WM_CREATE, "WM_CREATE"},
+    {WM_DESTROY, "WM_DESTROY"},
+    {WM_MOVE, "WM_MOVE"},
+    {WM_SIZE, "WM_SIZE"},
+    {WM_ACTIVATE, "WM_ACTIVATE"},
+    {WM_SETFOCUS, "WM_SETFOCUS"},
+    {WM_KILLFOCUS, "WM_KILLFOCUS"},
+    {WM_CLOSE, "WM_CLOSE"},
+    {WM_QUERYENDSESSION, "WM_QUERYENDSESSION"},
+    {WM_QUIT, "WM_QUIT"},
+    {WM_QUERYOPEN, "WM_QUERYOPEN"},
+    {WM_ERASEBKGND, "WM_ERASEBKGND"},
+    {WM_SYSCOLORCHANGE, "WM_SYSCOLORCHANGE"},
+    {WM_ENDSESSION, "WM_ENDSESSION"},
+    {WM_SHOWWINDOW, "WM_SHOWWINDOW"},
+    {WM_SETCURSOR, "WM_SETCURSOR"},
+    {WM_MOUSEACTIVATE, "WM_MOUSEACTIVATE"},
+    {WM_CHILDACTIVATE, "WM_CHILDACTIVATE"},
+    {WM_QUEUESYNC, "WM_QUEUESYNC"},
+    {WM_GETMINMAXINFO, "WM_GETMINMAXINFO"},
+    {WM_PAINTICON, "WM_PAINTICON"},
+    {WM_ICONERASEBKGND, "WM_ICONERASEBKGND"},
+    {WM_NEXTDLGCTL, "WM_NEXTDLGCTL"},
+    {WM_SETFONT, "WM_SETFONT"},
+    {WM_GETFONT, "WM_GETFONT"},
+    {WM_SETHOTKEY, "WM_SETHOTKEY"},
+    {WM_GETHOTKEY, "WM_GETHOTKEY"},
+    {WM_QUERYDRAGICON, "WM_QUERYDRAGICON"},
+    {WM_COMPAREITEM, "WM_COMPAREITEM"},
+    {WM_COMPACTING, "WM_COMPACTING"},
+    {WM_COMMNOTIFY, "WM_COMMNOTIFY"},
+    {WM_WINDOWPOSCHANGING, "WM_WINDOWPOSCHANGING"},
+    {WM_WINDOWPOSCHANGED, "WM_WINDOWPOSCHANGED"},
+    {WM_POWER, "WM_POWER"},
+    {WM_INPUTLANGCHANGEREQUEST, "WM_INPUTLANGCHANGEREQUEST"},
+    {WM_INPUTLANGCHANGE, "WM_INPUTLANGCHANGE"},
+    {WM_TCARD, "WM_TCARD"},
+    {WM_HELP, "WM_HELP"},
+    {WM_USERCHANGED, "WM_USERCHANGED"},
+    {WM_NOTIFYFORMAT, "WM_NOTIFYFORMAT"},
+    {WM_SETICON, "WM_SETICON"},
+    {WM_NCCREATE, "WM_NCCREATE"},
+    {WM_NCDESTROY, "WM_NCDESTROY"},
+    {WM_NCCALCSIZE, "WM_NCCALCSIZE"},
+    {WM_NCHITTEST, "WM_NCHITTEST"},
+    {WM_NCPAINT, "WM_NCPAINT"},
+    {WM_NCACTIVATE, "WM_NCACTIVATE"},
+    {WM_GETDLGCODE, "WM_GETDLGCODE"},
+    {WM_SYNCPAINT, "WM_SYNCPAINT"},
+    {WM_KEYDOWN, "WM_KEYDOWN"},
+    {WM_KEYFIRST, "WM_KEYFIRST"},
+    {WM_KEYUP, "WM_KEYUP"},
+    {WM_CHAR, "WM_CHAR"},
+    {WM_DEADCHAR, "WM_DEADCHAR"},
+    {WM_SYSKEYDOWN, "WM_SYSKEYDOWN"},
+    {WM_SYSKEYUP, "WM_SYSKEYUP"},
+    {WM_SYSCHAR, "WM_SYSCHAR"},
+    {WM_SYSDEADCHAR, "WM_SYSDEADCHAR"},
+    {WM_KEYLAST, "WM_KEYLAST"},
+    {WM_UNICHAR, "WM_UNICHAR"},
+    {WM_INITDIALOG, "WM_INITDIALOG"},
+    {WM_COMMAND, "WM_COMMAND"},
+    {WM_SYSCOMMAND, "WM_SYSCOMMAND"},
+    {WM_TIMER, "WM_TIMER"},
+    {WM_HSCROLL, "WM_HSCROLL"},
+    {WM_VSCROLL, "WM_VSCROLL"},
+    {WM_INITMENU, "WM_INITMENU"},
+    {WM_INITMENUPOPUP, "WM_INITMENUPOPUP"},
+    {WM_GESTURE, "WM_GESTURE"},
+    {WM_MENUCHAR, "WM_MENUCHAR"},
+    {WM_ENTERIDLE, "WM_ENTERIDLE"},
+    {WM_MENURBUTTONUP, "WM_MENURBUTTONUP"},
+    {WM_MENUDRAG, "WM_MENUDRAG"},
+    {WM_MENUGETOBJECT, "WM_MENUGETOBJECT"},
+    {WM_UNINITMENUPOPUP, "WM_UNINITMENUPOPUP"},
+    {WM_MENUCOMMAND, "WM_MENUCOMMAND"},
+    {WM_CHANGEUISTATE, "WM_CHANGEUISTATE"},
+    {WM_UPDATEUISTATE, "WM_UPDATEUISTATE"},
+    {WM_QUERYUISTATE, "WM_QUERYUISTATE"},
+    {WM_CTLCOLORMSGBOX, "WM_CTLCOLORMSGBOX"},
+    {WM_CTLCOLOREDIT, "WM_CTLCOLOREDIT"},
+    {WM_CTLCOLORLISTBOX, "WM_CTLCOLORLISTBOX"},
+    {WM_CTLCOLORBTN, "WM_CTLCOLORBTN"},
+    {WM_CTLCOLORDLG, "WM_CTLCOLORDLG"},
+    {WM_CTLCOLORSCROLLBAR, "WM_CTLCOLORSCROLLBAR"},
+    {WM_CTLCOLORSTATIC, "WM_CTLCOLORSTATIC"},
+    {WM_MOUSEFIRST, "WM_MOUSEFIRST"},
+    {WM_MOUSEMOVE, "WM_MOUSEMOVE"},
+    {WM_LBUTTONDOWN, "WM_LBUTTONDOWN"},
+    {WM_LBUTTONUP, "WM_LBUTTONUP"},
+    {WM_LBUTTONDBLCLK, "WM_LBUTTONDBLCLK"},
+    {WM_RBUTTONDOWN, "WM_RBUTTONDOWN"},
+    {WM_RBUTTONUP, "WM_RBUTTONUP"},
+    {WM_RBUTTONDBLCLK, "WM_RBUTTONDBLCLK"},
+    {WM_MBUTTONDOWN, "WM_MBUTTONDOWN"},
+    {WM_MBUTTONUP, "WM_MBUTTONUP"},
+    {WM_MBUTTONDBLCLK, "WM_MBUTTONDBLCLK"},
+    {WM_MOUSELAST, "WM_MOUSELAST"},
+    {WM_PARENTNOTIFY, "WM_PARENTNOTIFY"},
+    {WM_ENTERMENULOOP, "WM_ENTERMENULOOP"},
+    {WM_EXITMENULOOP, "WM_EXITMENULOOP"},
+    {WM_NEXTMENU, "WM_NEXTMENU"},
+    {WM_SIZING, "WM_SIZING"},
+    {WM_CAPTURECHANGED, "WM_CAPTURECHANGED"},
+    {WM_MOVING, "WM_MOVING"},
+    {WM_POWERBROADCAST, "WM_POWERBROADCAST"},
+    {WM_DEVICECHANGE, "WM_DEVICECHANGE"},
+    {WM_MDICREATE, "WM_MDICREATE"},
+    {WM_MDIDESTROY, "WM_MDIDESTROY"},
+    {WM_MDIACTIVATE, "WM_MDIACTIVATE"},
+    {WM_MDIRESTORE, "WM_MDIRESTORE"},
+    {WM_MDINEXT, "WM_MDINEXT"},
+    {WM_MDIMAXIMIZE, "WM_MDIMAXIMIZE"},
+    {WM_MDITILE, "WM_MDITILE"},
+    {WM_MDICASCADE, "WM_MDICASCADE"},
+    {WM_MDIICONARRANGE, "WM_MDIICONARRANGE"},
+    {WM_MDIGETACTIVE, "WM_MDIGETACTIVE"},
+    {WM_MDISETMENU, "WM_MDISETMENU"},
+    {WM_ENTERSIZEMOVE, "WM_ENTERSIZEMOVE"},
+    {WM_EXITSIZEMOVE, "WM_EXITSIZEMOVE"},
+    {WM_DROPFILES, "WM_DROPFILES"},
+    {WM_MDIREFRESHMENU, "WM_MDIREFRESHMENU"},
+    {WM_TOUCH, "WM_TOUCH"},
+    {WM_NCPOINTERUPDATE, "WM_NCPOINTERUPDATE"},
+    {WM_NCPOINTERDOWN, "WM_NCPOINTERDOWN"},
+    {WM_NCPOINTERUP, "WM_NCPOINTERUP"},
+    {WM_POINTERUPDATE, "WM_POINTERUPDATE"},
+    {WM_POINTERDOWN, "WM_POINTERDOWN"},
+    {WM_POINTERUP, "WM_POINTERUP"},
+    {WM_POINTERENTER, "WM_POINTERENTER"},
+    {WM_POINTERROUTEDTO, "WM_POINTERROUTEDTO"},
+    {WM_POINTERROUTEDAWAY, "WM_POINTERROUTEDAWAY"},
+    {WM_POINTERROUTEDRELEASED, "WM_POINTERROUTEDRELEASED"},
+    {WM_CUT, "WM_CUT"},
+    {WM_COPY, "WM_COPY"},
+    {WM_PASTE, "WM_PASTE"},
+    {WM_CLEAR, "WM_CLEAR"},
+    {WM_UNDO, "WM_UNDO"},
+    {WM_RENDERFORMAT, "WM_RENDERFORMAT"},
+    {WM_RENDERALLFORMATS, "WM_RENDERALLFORMATS"},
+    {WM_DESTROYCLIPBOARD, "WM_DESTROYCLIPBOARD"},
+    {WM_DRAWCLIPBOARD, "WM_DRAWCLIPBOARD"},
+    {WM_PAINTCLIPBOARD, "WM_PAINTCLIPBOARD"},
+    {WM_PALETTEISCHANGING, "WM_PALETTEISCHANGING"},
+    {WM_PALETTECHANGED, "WM_PALETTECHANGED"},
+    {WM_HOTKEY, "WM_HOTKEY"},
+    {WM_PRINT, "WM_PRINT"},
+    {WM_PRINTCLIENT, "WM_PRINTCLIENT"},
+    {WM_APPCOMMAND, "WM_APPCOMMAND"},
+    {WM_DWMCOLORIZATIONCOLORCHANGED, "WM_DWMCOLORIZATIONCOLORCHANGED"},
+    {WM_DWMWINDOWMAXIMIZEDCHANGE, "WM_DWMWINDOWMAXIMIZEDCHANGE"},
+    {WM_DWMSENDICONICTHUMBNAIL, "WM_DWMSENDICONICTHUMBNAIL"},
+    {WM_DWMSENDICONICLIVEPREVIEWBITMAP, "WM_DWMSENDICONICLIVEPREVIEWBITMAP"},
+    {WM_HANDHELDFIRST, "WM_HANDHELDFIRST"},
+    {WM_AFXFIRST, "WM_AFXFIRST"},
+    {WM_PENWINFIRST, "WM_PENWINFIRST"},
+    {WM_USER, "WM_USER"}
+};
 
+void printWindowPos(MSG* msg);
+
+void printSize(MSG* msg);
+
+void printSizing(MSG* msg);
+#endif
 RECT rectFromQRect(const QRect& rect)
 {
     return RECT {
@@ -315,6 +482,31 @@ bool ResizeEventFilter::nativeEventFilter(
         {
             return false;
         }
+#if YADAW_DEBUG_RESIZE_EVENT_FILTER
+        auto it = messages.find(LOWORD(msg->message));
+        if(it != messages.end())
+        {
+            if(it->first == WM_WINDOWPOSCHANGING || it->first == WM_WINDOWPOSCHANGED)
+            {
+                std::fprintf(stderr, "[DEBUG] %s\n", it->second);
+                printWindowPos(msg);
+            }
+            else if(it->first == WM_SIZE)
+            {
+                std::fprintf(stderr, "[DEBUG] %s\n", it->second);
+                printSize(msg);
+            }
+            else if(it->first == WM_SIZING)
+            {
+                std::fprintf(stderr, "[DEBUG] %s\n", it->second);
+                printSizing(msg);
+            }
+        }
+        else
+        {
+            std::fprintf(stderr, "[DEBUG] 0x%04X\n", LOWORD(msg->message));
+        }
+#endif
         if(msg->message == WM_NCLBUTTONDOWN
             && (msg->wParam >= HTLEFT && msg->wParam <= HTBOTTOMRIGHT))
         {
@@ -421,6 +613,13 @@ bool ResizeEventFilter::nativeEventFilter(
             }
         }
         prevIsCaptureChanged_ = msg->message == WM_CAPTURECHANGED;
+
+#if YADAW_DEBUG_RESIZE_EVENT_FILTER
+        if(msg->message == WM_WINDOWPOSCHANGING)
+        {
+            printWindowPos(msg);
+        }
+#endif
         return ret;
     }
 #elif __linux__
@@ -459,3 +658,62 @@ void ResizeEventFilter::windowPosChanged(MSG* msg)
 }
 #endif
 }
+#if _WIN32
+#if YADAW_DEBUG_RESIZE_EVENT_FILTER
+constexpr const char* swp[] = {
+    "SWP_NOSIZE",
+    "SWP_NOMOVE",
+    "SWP_NOZORDER",
+    "SWP_NOREDRAW",
+    "SWP_NOACTIVATE",
+    "SWP_FRAMECHANGED",
+    "SWP_SHOWWINDOW",
+    "SWP_HIDEWINDOW",
+    "SWP_NOCOPYBITS",
+    "SWP_NOOWNERZORDER",
+    "SWP_NOSENDCHANGING"
+};
+
+constexpr const char* sizeNames[] = {
+    "SIZE_RESTORED",
+    "SIZE_MINIMIZED",
+    "SIZE_MAXIMIZED",
+    "SIZE_MAXSHOW",
+    "SIZE_MAXHIDE"
+};
+
+constexpr const char* edges[] = {
+    "", "<-", "->", "^", "<^", "^>", "v", "<v", ">v"
+};
+
+void printWindowPos(MSG* msg)
+{
+    auto windowPos = reinterpret_cast<WINDOWPOS*>(msg->lParam);
+    std::fprintf(stderr, "[DEBUG]   Geometry: (%d, %d), %d x %d\n", windowPos->x, windowPos->y, windowPos->cx, windowPos->cy);
+    std::fprintf(stderr, "[DEBUG]     Flags: 0x%04X", windowPos->flags);
+    bool leftParen = false;
+    for(int i = 0; i < 11; ++i)
+    {
+        if(windowPos->flags & (1 << i))
+        {
+            std::fprintf(stderr, "%s%s", leftParen? " | ": "(", swp[i]);
+            leftParen = true;
+        }
+    }
+    std::fprintf(stderr, ")\n");
+}
+
+void printSize(MSG* msg)
+{
+    std::fprintf(stderr, "[DEBUG]   Type: %s\n", sizeNames[msg->wParam]);
+    std::fprintf(stderr, "[DEBUG]   Size: %d x %d\n", LOWORD(msg->lParam), HIWORD(msg->lParam));
+}
+
+void printSizing(MSG* msg)
+{
+    std::fprintf(stderr, "[DEBUG]   Edge: %s\n", edges[msg->wParam]);
+    auto rect = reinterpret_cast<RECT*>(msg->lParam);
+    std::fprintf(stderr, "[DEBUG]   Geometry: (%ld, %ld), %ld x %ld\n", rect->left, rect->top, rect->right - rect->left, rect->bottom - rect->top);
+}
+#endif
+#endif
